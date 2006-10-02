08e68c81d1b97c234f6830a3fd6402719da1cada
kobit <kobit@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2006-10-02 21:43:21 +0000

    Added new - non-standard stanza type used by kopete
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@126 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

--- src/tigase/xmpp/StanzaType.java
+++ src/tigase/xmpp/StanzaType.java
@@ -38,6 +38,7 @@ public enum StanzaType {
 		available, unavailable, probe, subscribe, // presence types
 		subscribed, unsubscribe, unsubscribed,    // presence types
 		chat, groupchat, headline, normal,        // message types
-		valid, invalid;                           // Dialback verification packets
+		valid, invalid,                           // Dialback verification packets
+    invisible;                                // Other unknown types...
 
 } // StanzaType
