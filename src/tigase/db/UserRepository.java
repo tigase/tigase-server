6b75c895158a7fa7f4558b8c6c6bd634a9d12807
kobit <kobit@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2006-10-10 21:41:40 +0000

    Added but commented out method: getInstance(resource) as there is still no consistent idea how it should work
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@140 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

--- src/tigase/db/UserRepository.java
+++ src/tigase/db/UserRepository.java
@@ -39,6 +39,21 @@ package tigase.db;
  */
 public interface UserRepository {
 
+// 	/**
+// 	 * <code>getInstance</code> method returns repository instance. This is to
+// 	 * multiple instances for the same resource like file. In such cases data
+// 	 * might become out of sync. So instead of creating instance using constructor
+// 	 * implementation of this interface must provide save function to access
+// 	 * repository data from different components/threads.
+// 	 *
+// 	 * @param resource a <code>String</code> value keeping data resource
+// 	 * representation. It is implementation dependant value and it might be a
+// 	 * filename, database connection string or whatever is required by
+// 	 * implementation.
+// 	 * @return an <code>UserRepository</code> value is a UserRepository instance.
+// 	 */
+// 	UserRepository getInstance(String resource);
+
 	/**
 	 * Method <code>userExists</code> checks whether specified user exists in
 	 * repository. Return <code>true</code> if user exists, <code>false</code>
