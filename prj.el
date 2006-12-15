3a050ef33524622a86e492c5497f581dea9806f3
kobit <kobit@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2006-12-15 12:38:34 +0000

    git-svn-id: file:///home/svn/repos/tigase-server/trunk@228 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

--- prj.el
+++ prj.el
@@ -1,28 +1,47 @@
 (jde-project-file-version "1.0")
-
 (jde-set-variables
  '(jde-electric-return-p t)
  '(jde-enable-abbrev-mode t)
+ '(jde-gen-k&r t)
+ '(indent-tabs-mode t)
+ '(user-mail-address "artur.hefczyc@gmail.com")
  '(jde-global-classpath
    (quote
-    (
-     "$JAVA_HOME/jre/lib/rt.jar"
+    ("$JAVA_HOME/jre/lib/rt.jar"
      "$PROJECTS_HOME/tigase/server/classes/"
      "$PROJECTS_HOME/tigase/server/libs/junit.jar"
      "$PROJECTS_HOME/tigase/server/libs/jdbc-mysql.jar"
      "$PROJECTS_HOME/tigase/server/libs/tigase-xmltools.jar"
-     "$PROJECTS_HOME/tigase/server/libs/tigase-utils.jar"
-     )))
+     "$PROJECTS_HOME/tigase/server/libs/tigase-utils.jar")))
  '(jde-sourcepath
    (quote
-    (
-     "$PROJECTS_HOME/tigase/server/src"
+    ("$PROJECTS_HOME/tigase/server/src"
      "$PROJECTS_HOME/tigase/xmltools/src"
      "$PROJECTS_HOME/tigase/utils/src"
      "$PROJECTS_HOME/tigase/server/tests/unittests/src"
-     "$JAVA_HOME/share/src"
-     "$JAVA_HOME/share/tests/src")))
- '(jde-gen-k&r t)
- '(user-mail-address "artur.hefczyc@tigase.org")
- '(indent-tabs-mode t)
-)
+     "$JAVA_HOME/share/src" "$JAVA_HOME/share/tests/src")))
+ '(jde-gen-buffer-boilerplate
+   (quote
+    ("/*  Package Jabber Server"
+     " *  Copyright (C) 2001, 2002, 2003, 2004, 2005"
+     " *  \"Artur Hefczyc\" <artur.hefczyc@tigase.org>"
+     " *"
+     " *  This program is free software; you can redistribute it and/or modify"
+     " *  it under the terms of the GNU General Public License as published by"
+     " *  the Free Software Foundation; either version 2 of the License, or"
+     " *  (at your option) any later version."
+     " *"
+     " *  This program is distributed in the hope that it will be useful,"
+     " *  but WITHOUT ANY WARRANTY; without even the implied warranty of"
+     " *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the"
+     " *  GNU General Public License for more details."
+     " *"
+     " *  You should have received a copy of the GNU General Public License"
+     " *  along with this program; if not, write to the Free Software Foundation,"
+     " *  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA."
+     " *"
+     " * $Rev: $"
+     " * Last modified by $Author: $"
+     " * $Date: $"
+     " */")))
+ )
