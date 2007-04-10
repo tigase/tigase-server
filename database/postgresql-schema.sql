eb16f5b2499f7d9651497c42e6ab3ba46c8e0fa5
kobit <kobit@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2007-04-10 14:28:05 +0000

    Updated user repository access files
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@380 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

--- database/postgresql-schema.sql
+++ database/postgresql-schema.sql
@@ -1,3 +1,35 @@
+--  Tigase database schema for PostgreSQL
+--
+--  Package Tigase XMPP/Jabber Server
+--  Copyright (C) 2004 - 2007
+--  "Artur Hefczyc" <artur.hefczyc@tigase.org>
+--
+--  This program is free software; you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation; either version 2 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program; if not, write to the Free Software Foundation,
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+--
+-- $Rev: 367 $
+-- Last modified by $Author: kobit $
+-- $Date: 2007-04-03 19:40:48 +0100 (Tue, 03 Apr 2007) $
+--
+
+--  To load schema to PostgreSQL database execute following commands:
+--
+--  createuser tigase
+--  createdb -U tigase tigase
+--  psql -q -U tigase -d tigase -f postgresql-schema.sql
+
+
 create table tig_users (
        uid bigint NOT NULL,
 
--- scripts/repo.sh
+++ scripts/repo.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-CP="jars/tigase-server.jar:/usr/share/jdbc-mysql/lib/jdbc-mysql.jar:libs/tigase-xmltools.jar:libs/tigase-utils.jar"
-D="-Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF-8 -Djdbc.drivers=com.mysql.jdbc.Driver"
+CP="jars/tigase-server.jar:/usr/share/jdbc-mysql/lib/jdbc-mysql.jar:libs/pg73jdbc3.jar:libs/tigase-xmltools.jar:libs/tigase-utils.jar"
+
+D="-Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF-8 -Djdbc.drivers=com.mysql.jdbc.Driver:org.postgresql.Driver"
+
+XML_REP="-sc tigase.db.xml.XMLRepository -su user-repository.xml"
+MYSQL_REP="-sc tigase.db.jdbc.JDBCRepository -su jdbc:mysql://localhost/tigase?user=root&password=mypass"
+PGSQL_REP="-sc tigase.db.jdbc.JDBCRepository -su jdbc:postgresql://localhost/tigase?user=tigase"
 
 java $D -cp $CP tigase.util.RepositoryUtils $*
