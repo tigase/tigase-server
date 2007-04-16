17876481b4b3eebb61cc5ad6e80d6ba9e64312ff
kobit <kobit@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2007-04-16 10:02:56 +0000

    Initial version of private storage implementation
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@394 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

---
+++ src/main/java/tigase/xmpp/impl/JabberIqPrivate.java
@@ -0,0 +1,174 @@
+/*  Package Jabber Server
+ *  Copyright (C) 2001-2007
+ *  "Artur Hefczyc" <artur.hefczyc@tigase.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software Foundation,
+ *  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * $Rev$
+ * Last modified by $Author$
+ * $Date$
+ */
+package tigase.xmpp.impl;
+
+import java.util.logging.Logger;
+import java.util.Queue;
+import java.util.List;
+import tigase.db.NonAuthUserRepository;
+import tigase.server.Packet;
+import tigase.xml.Element;
+import tigase.xml.SimpleParser;
+import tigase.xml.SingletonFactory;
+import tigase.xml.DomBuilderHandler;
+import tigase.xmpp.XMPPImplIfc;
+import tigase.xmpp.XMPPProcessor;
+import tigase.xmpp.XMPPProcessorIfc;
+import tigase.xmpp.XMPPResourceConnection;
+import tigase.xmpp.Authorization;
+import tigase.xmpp.StanzaType;
+import tigase.util.JID;
+import tigase.xmpp.NotAuthorizedException;
+import tigase.db.NonAuthUserRepository;
+import tigase.db.UserNotFoundException;
+
+/**
+ * Describe class JabberIqPrivate here.
+ *
+ *
+ * Created: Mon Apr 16 08:28:18 2007
+ *
+ * @author <a href="mailto:artur.hefczyc@tigase.org">Artur Hefczyc</a>
+ * @version $Rev$
+ */
+public class JabberIqPrivate extends XMPPProcessor implements XMPPProcessorIfc {
+
+	/**
+   * Private logger for class instancess.
+   */
+  private static Logger log =
+		Logger.getLogger("tigase.xmpp.impl.JabberIqPrivate");
+
+  protected static final String XMLNS = "jabber:iq:private";
+
+	private static final String PRIVATE_KEY = XMLNS;
+
+	protected static final String ID = XMLNS;
+	// VCARD element is added to support old vCard protocol where element
+	// name was all upper cases. Now the plugin should catch both cases.
+	protected static final String[] ELEMENTS = {"query"};
+  protected static final String[] XMLNSS = {XMLNS};
+
+	private static final SimpleParser parser =
+		SingletonFactory.getParserInstance();
+
+	// Implementation of tigase.xmpp.XMPPImplIfc
+
+	public String id() { return ID; }
+
+	public String[] supElements() { return ELEMENTS; }
+
+  public String[] supNamespaces() { return XMLNSS; }
+
+	// Implementation of tigase.xmpp.XMPPProcessorIfc
+
+	/**
+	 * Describe <code>process</code> method here.
+	 *
+	 * @param packet a <code>Packet</code> value
+	 * @param session a <code>XMPPResourceConnection</code> value
+	 * @param repo a <code>NonAuthUserRepository</code> value
+	 * @param results a <code>Queue</code> value
+	 */
+	public void process(Packet packet, XMPPResourceConnection session,
+		NonAuthUserRepository repo, Queue<Packet> results) {
+
+		// Don't do anything if session is null
+		if (session == null) {
+			log.info("Session null, dropping packet: " + packet.getStringData());
+			return;
+		} // end of if (session == null)
+
+		try {
+			String id = null;
+			if (packet.getElemTo() != null) {
+				id = JID.getNodeID(packet.getElemTo());
+			} // end of if (packet.getElemTo() != null)
+			// Yes, this packet is send by THIS user to the server
+			if (id == null || id.equals(session.getUserId())) {
+				List<Element> elems = packet.getElemChildren("/iq/query");
+				StanzaType type = packet.getType();
+				switch (type) {
+				case get:
+					if (elems != null && elems.size() > 0) {
+						Element elem = elems.get(0);
+						String priv = session.getData(PRIVATE_KEY, elem.getName(), null);
+						log.finest("Loaded private data for key: " + elem.getName()
+							+ ": " + priv);
+						if (priv != null) {
+							results.offer(parseXMLData(priv, packet));
+							break;
+						}
+					}
+					results.offer(packet.okResult((String)null, 2));
+					break;
+				case set:
+					if (packet.getFrom().equals(session.getConnectionId())) {
+						if (elems != null && elems.size() > 0) {
+							Element elem = elems.get(0);
+							log.finest("Saving private data: " + elem.toString());
+							session.setData(PRIVATE_KEY, elem.getName(), elem.toString());
+							results.offer(packet.okResult((String)null, 0));
+						}
+					} else {
+						results.offer(Authorization.NOT_AUTHORIZED.getResponseMessage(packet,
+								"You are not authorized to set vcard data.", true));
+					} // end of else
+					break;
+				case result:
+					// Should never happen, it is an error and should be ignored
+					break;
+				default:
+					results.offer(Authorization.BAD_REQUEST.getResponseMessage(packet,
+							"Request type is incorrect", false));
+					break;
+				} // end of switch (type)
+			} else {
+				Element result = packet.getElement().clone();
+				// According to spec we must set proper FROM attribute
+				//				result.setAttribute("from", session.getJID());
+				results.offer(new Packet(result));
+			} // end of else
+		} catch (NotAuthorizedException e) {
+      log.warning(
+				"Received privacy request but user session is not authorized yet: " +
+        packet.getStringData());
+			results.offer(Authorization.NOT_AUTHORIZED.getResponseMessage(packet,
+					"You must authorize session first.", true));
+		} // end of try-catch
+
+	}
+
+	private Packet parseXMLData(String data, Packet packet) {
+		DomBuilderHandler domHandler = new DomBuilderHandler();
+		parser.parse(domHandler, data.toCharArray(), 0, data.length());
+		Queue<Element> elems = domHandler.getParsedElements();
+		Packet result = packet.okResult((Element)null, 1);
+		Element query = result.getElement().findChild("/iq/query");
+		for (Element el: elems) {
+			query.addChild(el);
+		} // end of for (Element el: elems)
+		return result;
+	}
+
+}
