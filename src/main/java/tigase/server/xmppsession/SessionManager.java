661c7f44d22fc7287cf20143475db47d872c4880
kobit <kobit@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2007-06-05 21:45:21 +0000

    Removed extra, not used import
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@569 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

--- src/main/java/tigase/server/xmppsession/SessionManager.java
+++ src/main/java/tigase/server/xmppsession/SessionManager.java
@@ -29,7 +29,6 @@ import java.net.UnknownHostException;
 import java.security.Security;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
