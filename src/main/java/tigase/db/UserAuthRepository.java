484a54d5d1dfb4b637b63355d182347306bcdc2d
kobit <kobit@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2007-11-02 20:28:59 +0000

    JavaDoc comments added
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@710 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

--- src/main/java/tigase/db/UserAuthRepository.java
+++ src/main/java/tigase/db/UserAuthRepository.java
@@ -48,29 +48,127 @@ public interface UserAuthRepository {
 	public static final String DATA_KEY = "data";
 	public static final String USER_ID_KEY = "user-id";
 
+	/**
+	 * <code>queryAuth</code> returns mechanisms available for authentication.
+	 *
+	 * @param authProps a <code>Map</code> value with parameters for authentication.
+	 */
 	void queryAuth(Map<String, Object> authProps);
 
+	/**
+	 * <code>initRepository</code> method is doing initialization for database
+	 * connection. It may also do lazy initialization with database.
+	 * Connection to database might be established during the first authentication
+	 * request.
+	 *
+	 * @param conn_str a <code>String</code> value of database connection string.
+	 * The string must also contain database user name and password if required
+	 * for connection.
+	 * @exception DBInitException if an error occurs during access database. It won't
+	 * happen however as in this method we do simple variable assigment.
+	 */
 	void initRepository(String resource_uri) throws DBInitException;
 
+	/**
+	 * <code>getResourceUri</code> method returns database connection string.
+	 *
+	 * @return a <code>String</code> value of database connection string.
+	 */
 	String getResourceUri();
 
+	/**
+	 * <code>plainAuth</code> method performs non-sasl, plain authentication
+	 * as described in non-sasl authentication
+	 * <a href="http://www.xmpp.org/extensions/xep-0078.html">XEP-0078</a>.
+	 *
+	 * @param user a <code>String</code> value of user name
+	 * @param password a <code>String</code> value of plain user password.
+	 * @return a <code>boolean</code> value <code>true</code> on successful
+	 * authentication, <code>false</code> on authentication failure.
+	 * @exception UserNotFoundException if an given user name is not found in
+	 * the authentication repository.
+	 * @exception TigaseDBException if an error occurs during during accessing
+	 * database;
+	 * @exception AuthorizationException if an error occurs during authentication
+	 * process.
+	 */
 	boolean plainAuth(String user, String password)
 		throws UserNotFoundException, TigaseDBException, AuthorizationException;
 
+	/**
+	 * <code>plainAuth</code> method performs non-sasl, digest authentication
+	 * as described in non-sasl authentication
+	 * <a href="http://www.xmpp.org/extensions/xep-0078.html">XEP-0078</a>
+	 * For now it is empty and always returns <code>false</code> as I don't
+	 * have description for database with passwords.
+	 *
+	 * @param user a <code>String</code> value of user name
+	 * @param digest a <code>String</code> value password digest sum
+	 * @param id a <code>String</code> value session ID used for digest sum
+	 * calculation.
+	 * @param alg a <code>String</code> value of algorithm ID used for digest sum
+	 * calculation.
+	 * @return a <code>boolean</code> value <code>true</code> on successful
+	 * authentication, <code>false</code> on authentication failure.
+	 * @exception UserNotFoundException if an given user name is not found in
+	 * the authentication repository.
+	 * @exception TigaseDBException if an error occurs during during accessing
+	 * database;
+	 * @exception AuthorizationException if an error occurs during authentication
+	 * process.
+	 */
 	boolean digestAuth(String user, String digest, String id, String alg)
 		throws UserNotFoundException, TigaseDBException, AuthorizationException;
 
+	/**
+	 * Describe <code>otherAuth</code> method here.
+	 *
+	 * @param props a <code>Map</code> value
+	 * @return a <code>boolean</code> value
+	 * @exception UserNotFoundException if an error occurs
+	 * @exception TigaseDBException if an error occurs
+	 * @exception AuthorizationException if an error occurs
+	 */
 	boolean otherAuth(Map<String, Object> authProps)
 		throws UserNotFoundException, TigaseDBException, AuthorizationException;
 
+	/**
+	 * Describe <code>addUser</code> method here.
+	 *
+	 * @param user a <code>String</code> value
+	 * @param password a <code>String</code> value
+	 * @exception UserExistsException if an error occurs
+	 * @exception TigaseDBException if an error occurs
+	 */
 	void addUser(String user, String password)
 		throws UserExistsException, TigaseDBException;
 
+	/**
+	 * Describe <code>updatePassword</code> method here.
+	 *
+	 * @param user a <code>String</code> value
+	 * @param password a <code>String</code> value
+	 * @exception TigaseDBException if an error occurs
+	 */
   void updatePassword(String user, String password)
 		throws TigaseDBException;
 
+	/**
+	 * Describe <code>removeUser</code> method here.
+	 *
+	 * @param user a <code>String</code> value
+	 * @exception UserNotFoundException if an error occurs
+	 * @exception TigaseDBException if an error occurs
+	 */
   void removeUser(String user) throws UserNotFoundException, TigaseDBException;
 
+	/**
+	 * Describe <code>logout</code> method here.
+	 *
+	 * @param user a <code>String</code> value
+	 * @exception UserNotFoundException if an error occurs
+	 * @exception TigaseDBException if an error occurs
+	 */
 	void logout(String user) throws UserNotFoundException, TigaseDBException;
 
 } // UserAuthRepository
