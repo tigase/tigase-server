33e5f9157841d576481c41d1dd77c5ac6687f34d
kobit <kobit@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2007-12-22 18:48:35 +0000

    Code indetation corrected
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@800 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

--- prj.el
+++ prj.el
@@ -6,28 +6,32 @@
  '(indent-tabs-mode t)
  '(user-mail-address "artur.hefczyc@tigase.org")
  '(jde-global-classpath
-   (quote
-    ("$JAVA_HOME/jre/lib/rt.jar"
-     "$PROJECTS_HOME/tigase/server/classes/"
-     "$PROJECTS_HOME/tigase/server/libs/junit.jar"
-     "$PROJECTS_HOME/tigase/server/libs/jdbc-mysql.jar"
-     "$PROJECTS_HOME/tigase/server/libs/tigase-xmltools.jar"
-     "$PROJECTS_HOME/tigase/server/libs/tigase-utils.jar"
+	 (quote
+		(
+		 "$JAVA_HOME/jre/lib/rt.jar"
+		 "$PROJECTS_HOME/tigase/server/classes/"
+		 "$PROJECTS_HOME/tigase/server/libs/junit.jar"
+		 "$PROJECTS_HOME/tigase/server/libs/jdbc-mysql.jar"
+		 "$PROJECTS_HOME/tigase/server/libs/tigase-xmltools.jar"
+		 "$PROJECTS_HOME/tigase/server/libs/tigase-utils.jar"
 		 "$PROJECTS_HOME/tigase/server/libs/jml-1.0b2.jar"
 		 )))
  '(jde-sourcepath
-   (quote
-    (
+	 (quote
+		(
 		 "$PROJECTS_HOME/tigase/server/src/main/java"
-     "$PROJECTS_HOME/tigase/xmltools/src/main/java"
-     "$PROJECTS_HOME/tigase/utils/src/main/java"
-     "$PROJECTS_HOME/tigase/server/tests/unittests/src"
-     "$JAVA_HOME/share/src" "$JAVA_HOME/share/tests/src")))
+		 "$PROJECTS_HOME/tigase/xmltools/src/main/java"
+		 "$PROJECTS_HOME/tigase/utils/src/main/java"
+		 "$PROJECTS_HOME/tigase/server/tests/unittests/src"
+		 "$JAVA_HOME/share/src"
+		 "$JAVA_HOME/share/tests/src"
+		 )))
  '(jde-gen-buffer-boilerplate
-   (quote
-    ("/*  Tigase Jabber/XMPP Server"
-     " *  Copyright (C) 2004-2008 \"Artur Hefczyc\" <artur.hefczyc@tigase.org>"
-     " *"
+	 (quote
+		(
+		 "/*  Tigase Jabber/XMPP Server"
+		 " *  Copyright (C) 2004-2008 \"Artur Hefczyc\" <artur.hefczyc@tigase.org>"
+		 " *"
 		 " * This program is free software: you can redistribute it and/or modify"
 		 " * it under the terms of the GNU General Public License as published by"
 		 " * the Free Software Foundation, version 3 of the License."
@@ -40,9 +44,10 @@
 		 " * You should have received a copy of the GNU General Public License"
 		 " * along with this program. Look for COPYING file in the top folder."
 		 " * If not, see http://www.gnu.org/licenses/."
-     " *"
-     " * $Rev: $"
-     " * Last modified by $Author: $"
-     " * $Date: $"
-     " */")))
+		 " *"
+		 " * $Rev: $"
+		 " * Last modified by $Author: $"
+		 " * $Date: $"
+		 " */"
+		 )))
  )
