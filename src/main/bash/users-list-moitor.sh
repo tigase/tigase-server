243269566733022db8dce6f4ec826cb8fd92519a
kobit <kobit@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2008-07-02 17:29:16 +0000

    Added full path to adduser program which is needed when run from the cron job and also mail program is used to send the first greetings message to newly created user account
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@1080 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

--- src/main/bash/users-list-moitor.sh
+++ src/main/bash/users-list-moitor.sh
@@ -10,6 +10,15 @@ fi
 for i in $mydir/* ; do
   newuser=`basename $i`
   echo "Adding new user: "$newuser
-  useradd $newuser -m
+  /usr/sbin/useradd $newuser -m
   rm -f $i
+  /usr/bin/mail -s "Welcome to Tigase LiveCD" -t ${newuser}@livecd.tigase.org <<EOFMAIL
+Hi,
+
+This is automated messages sent to every new user registered on the LiveCD.
+Please enjoy the system and send me your comments.
+
+Artur Hefczyc
+.
+EOFMAIL
 done
