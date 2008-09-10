32b927748e0f64cb026bfe2855cd1da765b06755
kobit <kobit@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2008-09-10 18:54:40 +0000

    init(UserRepository rep) method added to allow custom repository configuration by plugins
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@1125 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

--- src/main/java/tigase/xmpp/XMPPImplIfc.java
+++ src/main/java/tigase/xmpp/XMPPImplIfc.java
@@ -21,6 +21,8 @@
  */
 package tigase.xmpp;
 
+import tigase.db.UserRepository;
+import tigase.db.TigaseDBException;
 import tigase.xml.Element;
 
 /**
@@ -34,16 +36,76 @@ import tigase.xml.Element;
  */
 public interface XMPPImplIfc {
 
+	/**
+	 * Method <code>id</code> returns a unique ID of the plugin.
+	 * Each plugin has own, unique ID which is used in the configuration file
+	 * to determine whether it needs to be loaded or not.
+	 * In most cases the ID can be equal to XMLNS of the packages processed
+	 * by the plugin.
+	 *
+	 * @return a <code>String</code> value
+	 */
 	String id();
 
+	/**
+	 * Method <code>supElements</code> returns an array of element names for stanzas
+	 * which can be processed by this plugin. Each element name corresponds to
+	 * XMLNS returned in array by <code>supNamespaces()</code> method.
+	 *
+	 * @return a <code>String[]</code> value
+	 */
 	String[] supElements();
 
+	/**
+	 * Method <code>supNamespaces</code> returns an array of namespaces for stanzas
+	 * which can be processed by this pluing. Each namespace corresponds to element
+	 * name returned in array by <code>supElemenets()</code> method.
+	 *
+	 * @return a <code>String[]</code> value
+	 */
 	String[] supNamespaces();
 
+	/**
+	 * Method <code>supStreamFeatures</code> returns an array of XML
+	 * <code>Element</code>s with stream features which have to be returned to
+	 * the client uppon request. Stream features returned by this method correspond
+	 * to features supported by this plugin.
+	 *
+	 * @param session a <code>XMPPResourceConnection</code> value
+	 * @return an <code>Element[]</code> value
+	 */
 	Element[] supStreamFeatures(XMPPResourceConnection session);
 
+	/**
+	 * Method <code>supDiscoFeatures</code> returns an array of XML
+	 * <code>Element</code>s with service discovery features which have to be
+	 * returned to the client uppon request. Service discovery features returned
+	 * by this method correspond to services supported by this plugin.
+	 *
+	 * @param session a <code>XMPPResourceConnection</code> value
+	 * @return an <code>Element[]</code> value
+	 */
 	Element[] supDiscoFeatures(XMPPResourceConnection session);
 
+	/**
+	 * Method <code>isSupporting</code> takes element name and namespace for this
+	 * element and determines whether this element can be processed by this plugin.
+	 *
+	 * @param elem a <code>String</code> value
+	 * @param ns a <code>String</code> value
+	 * @return a <code>boolean</code> value
+	 */
 	boolean isSupporting(String elem, String ns);
+
+	/**
+	 * Method <code>init</code> is called just after the plugin has been
+	 * loaded into memory. The idea behind this is to allow it to initialize
+	 * or check the database. This might be especially useful for plugins
+	 * which want to have a database access via non-standard stored procedures
+	 * or need schema upgrade.
+	 *
+	 * @param rep an <code>UserRepository</code> value
+	 */
+	void init(UserRepository rep) throws TigaseDBException;
 
 } // XMPPImplIfc
