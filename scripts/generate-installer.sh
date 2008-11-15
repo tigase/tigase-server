72c65a631a46edcc37ab105e1d832eac25b18b76
kobit <kobit@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2008-11-15 10:59:20 +0000

    Automatic version number update
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@1228 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

--- scripts/generate-installer.sh
+++ scripts/generate-installer.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-TIGVER=`grep -m 1 "Tigase-Version:" MANIFEST.MF | sed -e "s/Tigase-Version: \(.*\)/\\1/"`
+export TIGVER=`grep -m 1 "Tigase-Version:" MANIFEST.MF | sed -e "s/Tigase-Version: \(.*\)/\\1/"`
+sed -e "s/<appversion>\([^<]*\)<\/appversion>/<appversion>$TIGVER<\/appversion>/" \
+    src/main/izpack/install.xml > src/main/izpack/install_copy.xml
 
 /Applications/IzPack/bin/compile \
-     src/main/izpack/install.xml \
+     src/main/izpack/install_copy.xml \
      -h /Applications/IzPack/ \
      -b . -o ./packages/tigase-server-$TIGVER.jar
 
 python /Applications/IzPack/utils/izpack2exe/izpack2exe.py \
      --file=./packages/tigase-server-$TIGVER.jar --no-upx \
      --output=./packages/tigase-server-$TIGVER.exe
+
+rm -f src/main/izpack/install_copy.xml
