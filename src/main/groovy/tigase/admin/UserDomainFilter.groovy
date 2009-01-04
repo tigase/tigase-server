925fc4ff6a66239628537a0767e84e3a26e432b9
kobit <kobit@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2009-01-04 00:13:10 +0000

    2 example Groovy scripts for admin commands.
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@1381 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

---
+++ src/main/groovy/tigase/admin/UserDomainFilter.groovy
@@ -0,0 +1,39 @@
+/*
+ * Tigase Jabber/XMPP Server
+ * Copyright (C) 2004-2008 "Artur Hefczyc" <artur.hefczyc@tigase.org>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://www.gnu.org/licenses/.
+ * 
+ * $Rev: $
+ * Last modified by $Author: $
+ * $Date: $
+ */
+
+package tigase.admin
+
+import tigase.server.Command
+import tigase.server.Packet
+
+Packet p = (Packet)packet
+num1 = Command.getFieldValue(p, "num1")
+num2 = Command.getFieldValue(p, "num2")
+
+if (num1 == null || num2 == null) {
+	Packet res = p.commandResult(Command.DataType.form);
+	Command.addFieldValue(res, "num1", "")
+	Command.addFieldValue(res, "num2", "")
+	return res
+}
+
+return num1 + num2
