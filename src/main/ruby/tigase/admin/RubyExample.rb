094688ae0f9d7fab637db1ff2367caccc49bba1f
kobit <kobit@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2009-01-04 00:14:58 +0000

    Example ruby script for a scripting admin command.
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@1384 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

---
+++ src/main/ruby/tigase/admin/RubyExample.rb
@@ -0,0 +1,40 @@
+# Tigase Jabber/XMPP Server
+# Copyright (C) 2004-2008 "Artur Hefczyc" <artur.hefczyc@tigase.org>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program. Look for COPYING file in the top folder.
+# If not, see http://www.gnu.org/licenses/.
+# 
+# $Rev: $
+# Last modified by $Author: $
+# $Date: $
+
+require 'java'
+
+import java.lang.System
+
+include_class 'tigase.server.Command'
+include_class 'tigase.server.Packet'
+
+
+num1 = Command.getFieldValue($packet, "num1")
+num2 = Command.getFieldValue($packet, "num2")
+
+if num1.nil? || num2.nil?
+   res = Packet.commandResultForm($packet)
+   Command.addTextField(res, "Note", "This is JRuby script!")
+	 Command.addFieldValue(res, "num1", "")
+   Command.addFieldValue(res, "num2", "")
+   return res
+else
+  return num1 + num2
+end
