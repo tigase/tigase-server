ab80f1176c49291e864a11bf94a4796c0eefb216
kobit <kobit@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2009-01-10 22:43:23 +0000

    Interface for utlity class setting up monitorings.
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@1410 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

---
+++ src/main/java/tigase/conf/MonitoringSetupIfc.java
@@ -0,0 +1,43 @@
+/*
+ * Tigase Jabber/XMPP Server
+ * Copyright (C) 2004-2008 "Artur Hefczyc" <artur.hefczyc@tigase.org>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://www.gnu.org/licenses/.
+ * 
+ * $Rev$
+ * Last modified by $Author$
+ * $Date$
+ */
+
+package tigase.conf;
+
+/**
+ * Created: Jan 10, 2009 8:43:04 PM
+ *
+ * @author <a href="mailto:artur.hefczyc@tigase.org">Artur Hefczyc</a>
+ * @version $Rev$
+ */
+public interface MonitoringSetupIfc {
+	
+	public enum MONITOR { jmx, http, snmp; }
+	
+	void initMonitoring(String settings, String configDir);
+
+	void initializationCompleted();
+
+	void putMXBean(String objName, Object bean);
+
+	Object getMXBean(String objName);
+
+}
