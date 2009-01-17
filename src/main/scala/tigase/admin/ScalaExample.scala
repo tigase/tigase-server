71c0b8ce42ed702d277368e6113b8211299d2ce7
kobit <kobit@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2009-01-17 15:47:30 +0000

    Example scala script. It is not yet working with the tigase server. Might be a problem with JSR-223 implementation.
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@1427 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

---
+++ src/main/scala/tigase/admin/ScalaExample.scala
@@ -0,0 +1,38 @@
+/*
+ * Tigase Jabber/XMPP Server
+ * Copyright (C) 2004-2008 "Artur Hefczyc" <artur.hefczyc@tigase.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ * $Rev: $
+ * Last modified by $Author: $
+ * $Date: $
+ */
+
+package tigase.admin
+
+import tigase.server. { Command, Packet }
+
+val num1 = Command getFieldValue(packet, "num1")
+val num2 = Command getFieldValue(packet, "num2")
+
+scriptResult = if (num1 == null || num2 == null) {
+	val cmd = Packet.commandResultForm(packet)
+	Command.addTextField(cmd, "Note", "This is a Scala script!")
+	Command.addFieldValue(cmd, "num1", "")
+	Command.addFieldValue(cmd, "num2", "")
+	cmd
+} else {
+	num1 + num2
+}
