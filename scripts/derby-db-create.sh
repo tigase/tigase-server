9237f4aa186d7e09a735796eedbadc1b36cd2082
kobit <kobit@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2009-01-27 12:14:40 +0000

    A simple shell script creating derby database in a given location.
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@1458 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

---
+++ scripts/derby-db-create.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+[[ "$1" = "" ]] && \
+  echo "Give me a path to the location where you want to have the database created" && \
+  exit 1
+
+
+java -Dij.protocol=jdbc:derby: -Dij.database="$1/tigasedb;create=true" \
+		-Dderby.system.home=`pwd` \
+		-cp libs/derby.jar:libs/derbytools.jar:jars/tigase-server.jar \
+		org.apache.derby.tools.ij database/derby-schema-4.sql
+java -Dij.protocol=jdbc:derby: -Dij.database="$1/tigasedb" \
+		-Dderby.system.home=`pwd` \
+		-cp libs/derby.jar:libs/derbytools.jar:jars/tigase-server.jar \
+		org.apache.derby.tools.ij database/derby-schema-4-sp.schema
+java -Dij.protocol=jdbc:derby: -Dij.database="$1/tigasedb" \
+		-Dderby.system.home=`pwd` \
+		-cp libs/derby.jar:libs/derbytools.jar:jars/tigase-server.jar \
+		org.apache.derby.tools.ij database/derby-schema-4-props.sql
+
