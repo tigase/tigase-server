b91ee25945c0acfa24a74f78dc9e0da45024c5c7
kobit <kobit@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2009-02-19 23:17:31 +0000

    Initial version for the server active monitoring code.
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@1520 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

---
+++ src/main/java/tigase/sys/ResourceState.java
@@ -0,0 +1,35 @@
+/*
+ * Tigase Jabber/XMPP Server
+ * Copyright (C) 2004-2008 "Artur Hefczyc" <artur.hefczyc@tigase.org>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://www.gnu.org/licenses/.
+ * 
+ * $Rev$
+ * Last modified by $Author$
+ * $Date$
+ */
+
+package tigase.sys;
+
+/**
+ * Created: Feb 19, 2009 12:22:55 PM
+ *
+ * @author <a href="mailto:artur.hefczyc@tigase.org">Artur Hefczyc</a>
+ * @version $Rev$
+ */
+public enum ResourceState {
+	GREEN,
+	YELLOW,
+	RED;
+}
