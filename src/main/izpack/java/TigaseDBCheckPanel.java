0928474bcf1971a5bfd2ad5fe5f2cf96c3963d10
kobit <kobit@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2009-03-10 13:19:07 +0000

    Corrected license text.
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@1526 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

--- src/main/izpack/java/TigaseDBCheckPanel.java
+++ src/main/izpack/java/TigaseDBCheckPanel.java
@@ -1,22 +1,23 @@
 /*
- * IzPack - Copyright 2001-2008 Julien Ponge, All Rights Reserved.
- * 
- * http://izpack.org/
- * http://izpack.codehaus.org/
- * 
- * Copyright 2002 Jan Blok
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0
- *     
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Tigase Jabber/XMPP Server
+ * Copyright (C) 2004-2007 "Artur Hefczyc" <artur.hefczyc@tigase.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ * $Rev$
+ * Last modified by $Author$
+ * $Date$
  */
 
 package com.izforge.izpack.panels;
@@ -60,7 +61,8 @@ import java.sql.Types;
 /**
  * The Hello panel class.
  *
- * @author Julien Ponge
+ * @author <a href="mailto:artur.hefczyc@tigase.org">Artur Hefczyc</a>
+ * @version $Rev$
  */
 public class TigaseDBCheckPanel extends IzPanel {
 
