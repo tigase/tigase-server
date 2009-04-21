dac34ce4b9310eaf74190566fa07d80d3150f0af
kobit <kobit@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2009-04-21 17:30:25 +0000

    Added support for SunOS
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@1570 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

--- src/main/java/tigase/server/xmppsession/PacketFilter.java
+++ src/main/java/tigase/server/xmppsession/PacketFilter.java
@@ -212,15 +212,15 @@ public class PacketFilter {
 					results.offer(Authorization.FEATURE_NOT_IMPLEMENTED.getResponseMessage(
 									packet, "Features not implemented yet.", true));
 				} else {
-    				if (log.isLoggable(Level.FINEST)) {
-        				log.finest("Yes, this is packet to 'this' client: " + id);
-                    }
+					if (log.isLoggable(Level.FINEST)) {
+						log.finest("Yes, this is packet to 'this' client: " + id);
+					}
 					Element elem = packet.getElement().clone();
 					Packet result = new Packet(elem);
 					result.setTo(session.getConnectionId(packet.getElemTo()));
-    				if (log.isLoggable(Level.FINEST)) {
-        				log.finest("Setting to: " + result.getTo());
-                    }
+					if (log.isLoggable(Level.FINEST)) {
+						log.finest("Setting to: " + result.getTo());
+					}
 					result.setFrom(packet.getTo());
 					results.offer(result);
 				}
