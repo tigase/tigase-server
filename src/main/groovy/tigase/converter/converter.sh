161b8671e572e2515c95b73b597f18f64fe8a38b
smoku <smoku@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2009-05-05 22:04:19 +0000

    Do not use Groovy jar provided by tigase
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@1608 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

--- src/main/groovy/tigase/converter/converter.sh
+++ src/main/groovy/tigase/converter/converter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 LIBS="/usr/share/tigase-server/lib /usr/share/openfire/lib"
-for DIR in $LIBS; do CLASSPATH="`ls -d $DIR/*.jar 2>/dev/null | tr '\n' :`$CLASSPATH"; done
+for DIR in $LIBS; do CLASSPATH="`ls -d $DIR/*.jar 2>/dev/null | grep -v groovy | tr '\n' :`$CLASSPATH"; done
 export CLASSPATH
 #echo $CLASSPATH
 exec ${0/%.sh/.groovy}
