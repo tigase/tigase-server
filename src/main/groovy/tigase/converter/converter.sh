3e7226dcf14d4c3de63d6f2acfa8afa8c5b5d1ce
smoku <smoku@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2009-05-06 23:55:07 +0000

    Check for UTF8 based locale.
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@1613 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

--- src/main/groovy/tigase/converter/converter.sh
+++ src/main/groovy/tigase/converter/converter.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 LIBS="/usr/share/tigase-server/lib /usr/share/openfire/lib"
+
+# Locale check
+if ! locale | grep -iq '^LC_CTYPE=.*\.utf8.\?$'; then
+	echo "You need to set UTF8 based locale first" >&2; exit 1
+fi
+
 for DIR in $LIBS; do CLASSPATH="`ls -d $DIR/*.jar 2>/dev/null | grep -v groovy | tr '\n' :`$CLASSPATH"; done
 export CLASSPATH
 #echo $CLASSPATH
