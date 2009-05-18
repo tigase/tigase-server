460687c994291722a1d74ac2a4f7837197140b63
smoku <smoku@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2009-05-18 13:37:35 +0000

    Using tr -d to delete newlines
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@1638 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

--- src/main/groovy/tigase/converter/converter.sh
+++ src/main/groovy/tigase/converter/converter.sh
@@ -6,7 +6,7 @@ if ! locale | grep -iq '^LC_CTYPE=.*\.utf8.\?$'; then
 	echo "You need to set UTF8 based locale first" >&2; exit 1
 fi
 
-for DIR in $LIBS; do CLASSPATH="`ls -d $DIR/*.jar 2>/dev/null | grep -v groovy | tr '\n' :`$CLASSPATH"; done
+for DIR in $LIBS; do CLASSPATH="`ls -d $DIR/*.jar 2>/dev/null | grep -v groovy | tr -d '\n' :`$CLASSPATH"; done
 export CLASSPATH
 #echo $CLASSPATH
 exec ${0/%.sh/.groovy}
