76524d26c4b560b7b13798e97ee054cffe955fd1
kobit <kobit@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2009-07-06 19:09:52 +0000

    JavaDoc added.
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@1760 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

--- src/main/java/tigase/cluster/ClusteringStrategyIfc.java
+++ src/main/java/tigase/cluster/ClusteringStrategyIfc.java
@@ -37,20 +37,87 @@ public interface ClusteringStrategyIfc {
 
 	//void init(String smName);
 
+	/**
+	 * This method is used for configuration purpose. Following the convention
+	 * used in the Tigase project this method is supposed to provide configuration
+	 * defaults. All parameters which exist in configuration file overwrite
+	 * corresponding default parameters. If some parameters are missing in
+	 * configuration file defaults are used then.
+	 *
+	 * A compiled set of parameters is then passed to <code>setProperties</code>
+	 * method.
+	 * @param params a <code>Map</code> with properties loaded from
+	 * init.properties file which should be used for generating defaults.
+	 * @return a <code>Map</code> with all the class default configuration
+	 * parameters.
+	 */
 	Map<String, Object> getDefaults(Map<String, Object> params);
 
+	/**
+	 * Method used to pass configuration parameters to the class. Parameters are
+	 * stored in <code>Map</code> which contains compiles set of defaults overwriten
+	 * by parameters loaded from configuration file.
+	 *
+	 * If he implementation took a good care of providing defaults for all
+	 * parameters no parameter should be missing.
+	 * @param props a <code>Map</code> with all configuration parameters for the class.
+	 */
 	void setProperties(Map<String, Object> props);
 
+	/**
+	 * Returns a <code>List</code> of all cluster nodes on which the given user session
+	 * can exist. In the simplest scenario it can always return all cluster nodes
+	 * as in theory the user can connect to any node if it is not on the local node.
+	 * More specialized implementation can know kind of hashing algorithm which is
+	 * used for binding a specific user to a specific node or the implementation
+	 * can keep track of all connected users to all nodes and 'know' where is
+	 * the user connected at any given time.
+	 *
+	 * In theory it can also return <code>'null'</code> it it 'knows' the user is
+	 * offline.
+	 * @param jid is a user full JID.
+	 * @return List of cluster nodes to which the user can be connected.
+	 */
 	List<String> getNodesForJid(String jid);
 
+	/**
+	 * The method retutns all cluster nodes currently connected to the cluster.
+	 * @return List of all cluster nodes currently connected to the cluster.
+	 */
 	List<String> getAllNodes();
 
-	void nodeConnected(String jid);
+	/**
+	 * This is a handler method which is called when a new node connects to
+	 * the cluster.
+	 * @param node is a cluster node id.
+	 */
+	void nodeConnected(String node);
 
-	void nodeDisconnected(String jid);
+	/**
+	 * This is a handler method which is called when a node disconnects from
+	 * the cluster.
+	 * @param node is a cluster node id.
+	 */
+	void nodeDisconnected(String node);
 
-	void userConnected(String jid, String sm, Queue<Packet> results);
+	/**
+	 * This is a handler method which is called when a user connects to some
+	 * node in the cluster.
+	 * @param jid is a user full JID just connected to the cluster.
+	 * @param node is a cluster node id where the jid is connected to.
+	 * @param results is a collection of packets which can be generated upon the
+	 * user connection by the implementation.
+	 */
+	void userConnected(String jid, String node, Queue<Packet> results);
 
-	void userDisconnected(String jid, String sm, Queue<Packet> results);
+	/**
+	 * This is a handler method which is called when a user disconnects from some
+	 * node in the cluster.
+	 * @param jid us a user full JID just disconnected from the cluster.
+	 * @param node is a cluster node id from which the user disconnected.
+	 * @param results is a collection of packets which can be generated upon the
+	 * user disconnection by the implementation.
+	 */
+	void userDisconnected(String jid, String node, Queue<Packet> results);
 
 }
