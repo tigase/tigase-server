912b18296124048a7f975f48f08fdf859e0e7c77
kobit <kobit@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2009-07-16 21:58:00 +0000

    Identity (reference) comparator to speed HashMaps processing for JIDs cache.
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@1768 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

---
+++ src/main/java/tigase/util/IdentityComparator.java
@@ -0,0 +1,46 @@
+/*
+ * Tigase Jabber/XMPP Server
+ * Copyright (C) 2004-2008 "Artur Hefczyc" <artur.hefczyc@tigase.org>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://www.gnu.org/licenses/.
+ * 
+ * $Rev$
+ * Last modified by $Author$
+ * $Date$
+ */
+
+package tigase.util;
+
+import java.util.Comparator;
+
+/**
+ * Created: Jul 15, 2009 11:50:51 AM
+ *
+ * @author <a href="mailto:artur.hefczyc@tigase.org">Artur Hefczyc</a>
+ * @version $Rev$
+ */
+public class IdentityComparator implements Comparator<Object> {
+
+	@Override
+	public int compare(Object o1, Object o2) {
+		if (o1 == o2) {
+			return 0;
+		}
+		if (o1.hashCode() < o2.hashCode()) {
+			return -1;
+		}
+		return 1;
+	}
+
+}
