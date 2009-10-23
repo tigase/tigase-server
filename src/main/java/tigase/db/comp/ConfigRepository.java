575bfba76677f42ff24abdf259d3a23ee8cfe4cd
kobit <kobit@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2009-10-23 19:17:17 +0000

    A new API for storing common data in the database. Most suitable for storring kind of extended configuration options, like a list of virtual domains with options, MUC rooms, external components etc...
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@1875 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

---
+++ src/main/java/tigase/db/comp/ConfigRepository.java
@@ -0,0 +1,126 @@
+/*
+ * Tigase Jabber/XMPP Server
+ * Copyright (C) 2004-2008 "Artur Hefczyc" <artur.hefczyc@tigase.org>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://www.gnu.org/licenses/.
+ * 
+ * $Rev$
+ * Last modified by $Author$
+ * $Date$
+ */
+
+package tigase.db.comp;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+import tigase.db.ComponentRepository;
+import tigase.db.RepositoryItem;
+
+/**
+ * Created: Oct 3, 2009 2:58:41 PM
+ *
+ * @param <Item>
+ * @author <a href="mailto:artur.hefczyc@tigase.org">Artur Hefczyc</a>
+ * @version $Rev$
+ */
+public abstract class ConfigRepository<Item extends RepositoryItem>
+		implements ComponentRepository<Item> {
+
+	private static final Logger log = Logger.getLogger(ConfigRepository.class.getName());
+
+	public static final String ANONYMOUS_DOMAINS_PROP_KEY = "anonymous-domains";
+
+	protected LinkedHashMap<String, Item> items = new LinkedHashMap<String, Item>();
+
+	public abstract String[] getDefaultPropetyItems();
+
+	public abstract String getPropertyKey();
+	
+	public abstract String getConfigKey();
+
+	public abstract Item getItemInstance();
+
+	@Override
+	public void getDefaults(Map<String, Object> defs,
+			Map<String, Object> params) {
+		String[] items_arr = getDefaultPropetyItems();
+		if (params.get(getPropertyKey()) != null) {
+			items_arr = ((String) params.get(getPropertyKey())).split(",");
+		}
+		defs.put(getConfigKey(), items_arr);
+	}
+
+	@Override
+	public void setProperties(Map<String, Object> properties) {
+		String[] items_arr = (String[]) properties.get(getConfigKey());
+		if (items_arr != null && items_arr.length > 0) {
+			items.clear();
+			for (String it : items_arr) {
+				log.config("Loading config item: " + it);
+				Item item = getItemInstance();
+				item.initFromPropertyString(it);
+				items.put(item.getKey(), item);
+			}
+		} else {
+			log.warning("Items list is not set in the configuration file!!");
+		}
+	}
+
+	@Override
+	public Item getItem(String key) {
+		return items.get(key);
+	}
+
+	@Override
+	public boolean contains(String key) {
+		return items.keySet().contains(key);
+	}
+
+	@Override
+	public void reload() { }
+
+	@Override
+	public void store() { }
+
+	@Override
+	public int size() {
+		return items.size();
+	}
+
+	@Override
+	public void addItem(Item item) {
+		items.put(item.getKey(), item);
+		store();
+	}
+
+	@Override
+	public void removeItem(String key) {
+		items.remove(key);
+		store();
+	}
+
+	@Override
+	public Collection<Item> allItems() {
+		return items.values();
+	}
+
+	@Override
+	public Iterator<Item> iterator() {
+		return items.values().iterator();
+	}
+
+}
---
+++ src/main/java/tigase/db/comp/UserRepoRepository.java
@@ -0,0 +1,159 @@
+/*
+ * Tigase Jabber/XMPP Server
+ * Copyright (C) 2004-2008 "Artur Hefczyc" <artur.hefczyc@tigase.org>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://www.gnu.org/licenses/.
+ * 
+ * $Rev$
+ * Last modified by $Author$
+ * $Date$
+ */
+
+package tigase.db.comp;
+
+import java.util.Map;
+import java.util.Queue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import tigase.db.RepositoryFactory;
+import tigase.db.RepositoryItem;
+import tigase.db.UserExistsException;
+import tigase.db.UserRepository;
+import tigase.xml.DomBuilderHandler;
+import tigase.xml.Element;
+import tigase.xml.SimpleParser;
+import tigase.xml.SingletonFactory;
+import static tigase.conf.Configurable.*;
+
+/**
+ * Created: Oct 3, 2009 3:55:27 PM
+ *
+ * @param <Item>
+ * @author <a href="mailto:artur.hefczyc@tigase.org">Artur Hefczyc</a>
+ * @version $Rev$
+ */
+public abstract class UserRepoRepository<Item extends RepositoryItem>
+		extends ConfigRepository<Item> {
+
+	private static final Logger log =
+			Logger.getLogger(UserRepoRepository.class.getName());
+	public static final String REPO_CLASS_PROP_KEY = "repo-class";
+	public static final String REPO_URI_PROP_KEY = "repo-uri";
+	private String items_list_pkey = "items-lists";
+
+	private UserRepository repo = null;
+
+	public abstract String getRepoUser();
+
+	public String getItemsListPKey() {
+		return items_list_pkey;
+	}
+
+	@Override
+	public void getDefaults(Map<String, Object> defs, Map<String, Object> params) {
+		// Something to initialize database with, in case it is empty
+		// Otherwise the server would not work at all with empty Items database
+		super.getDefaults(defs, params);
+
+		// Now the real items data storage:
+		String repo_class = DERBY_REPO_CLASS_PROP_VAL;
+		String repo_uri = DERBY_REPO_URL_PROP_VAL;
+		if (params.get(GEN_USER_DB) != null) {
+			repo_class = (String)params.get(GEN_USER_DB);
+		}
+		if (params.get(GEN_USER_DB_URI) != null) {
+			repo_uri = (String)params.get(GEN_USER_DB_URI);
+		}
+		defs.put(REPO_CLASS_PROP_KEY, repo_class);
+		defs.put(REPO_URI_PROP_KEY, repo_uri);
+	}
+
+	@Override
+	public void setProperties(Map<String, Object> properties) {
+		// Let's load items from configuration first. Later we can overwrite
+		// them with items settings in the database.
+		super.setProperties(properties);
+
+		repo = (UserRepository) properties.get(SHARED_USER_REPO_PROP_KEY);
+		if (repo != null) {
+			log.config("Using shared repository instance.");
+		} else {
+			String repo_class = (String) properties.get(REPO_CLASS_PROP_KEY);
+			String repo_uri = (String) properties.get(REPO_URI_PROP_KEY);
+			try {
+				repo = RepositoryFactory.getUserRepository(getRepoUser(), repo_class,
+						repo_uri, null);
+			} catch (Exception e) {
+				log.log(Level.SEVERE, "Can't initialize Items repository", e);
+				repo = null;
+			}
+		}
+		if (repo != null) {
+			// If this is the first run of the Items manager the database might not
+			// be properly initialized yet....
+			try {
+				repo.addUser(getRepoUser());
+			} catch (UserExistsException e) {
+				// This is expected when the Items repository has been already running on
+				// this databaseso and can be ignored.
+			} catch (Exception e) {
+				// This is not expected so let's signal an error:
+				log.log(Level.SEVERE,
+						"Problem with adding '" + getRepoUser() + "' user to the database", e);
+			}
+			reload();
+		}
+	}
+
+	@Override
+	public void reload() {
+		super.reload();
+		try {
+			// It is now time to load all Items' settings from the database:
+			String items_list = repo.getData(getRepoUser(), getItemsListPKey());
+			if (items_list != null && !items_list.isEmpty()) {
+				DomBuilderHandler domHandler = new DomBuilderHandler();
+				SimpleParser parser = SingletonFactory.getParserInstance();
+				parser.parse(domHandler, items_list.toCharArray(), 0,
+						items_list.length());
+				Queue<Element> elems = domHandler.getParsedElements();
+				if (elems != null && elems.size() > 0) {
+					for (Element elem : elems) {
+						Item item = getItemInstance();
+						item.initFromElement(elem);
+						items.put(item.getKey(), item);
+					}
+				}
+			}
+		} catch (Exception ex) {
+			log.log(Level.SEVERE,
+					"Problem with loading items list from the database.", ex);
+		}
+	}
+
+	@Override
+	public void store() {
+		super.store();
+		StringBuilder sb = new StringBuilder();
+		for (Item item : items.values()) {
+			sb.append(item.toElement().toString());
+		}
+		try {
+			repo.setData(getRepoUser(), getItemsListPKey(), sb.toString());
+		} catch (Exception e) {
+			log.log(Level.SEVERE, "Error storing items list in the repository", e);
+		}
+	}
+
+}
