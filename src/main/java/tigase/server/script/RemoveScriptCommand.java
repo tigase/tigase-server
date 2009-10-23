0e0010d97d4ccea5d5474d2527dfe50fff7a9f0b
kobit <kobit@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2009-10-23 19:38:54 +0000

    Changes for a new service discovery and scripting API.
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@1893 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

---
+++ src/main/java/tigase/server/script/RemoveScriptCommand.java
@@ -0,0 +1,92 @@
+/*
+ * Tigase Jabber/XMPP Server
+ * Copyright (C) 2004-2008 "Artur Hefczyc" <artur.hefczyc@tigase.org>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://www.gnu.org/licenses/.
+ * 
+ * $Rev$
+ * Last modified by $Author$
+ * $Date$
+ */
+
+package tigase.server.script;
+
+import tigase.server.script.CommandIfc;
+import tigase.server.script.AbstractScriptCommand;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+import javax.script.Bindings;
+import tigase.disco.ServiceEntity;
+import tigase.server.Command;
+import tigase.server.Packet;
+
+/**
+ * Created: Jan 2, 2009 2:30:41 PM
+ *
+ * @author <a href="mailto:artur.hefczyc@tigase.org">Artur Hefczyc</a>
+ * @version $Rev$
+ */
+public class RemoveScriptCommand extends AbstractScriptCommand {
+
+	@Override
+	@SuppressWarnings({"unchecked"})
+	public void runCommand(Packet packet, Bindings binds, Queue<Packet> results) {
+		String commandId = Command.getFieldValue(packet, COMMAND_ID);
+		if (isEmpty(commandId)) {
+			results.offer(prepareScriptCommand(packet, binds));
+		} else {
+			Map<String, CommandIfc> adminCommands =
+							(Map<String, CommandIfc>) binds.get(ADMN_CMDS);
+			adminCommands.remove(commandId);
+			ServiceEntity serviceEntity = (ServiceEntity) binds.get(ADMN_DISC);
+			ServiceEntity item =
+							serviceEntity.findNode("http://jabber.org/protocol/admin#" +
+							commandId);
+			serviceEntity.removeItems(item);
+			Packet result = packet.commandResult(Command.DataType.result);
+			Command.addTextField(result, "Note",
+							"There is no command script to remove");
+			results.offer(result);
+		}
+	}
+
+	@SuppressWarnings({"unchecked"})
+	private Packet prepareScriptCommand(Packet packet, Bindings binds) {
+		Packet result = null;
+		Map<String, CommandIfc> adminCommands =
+						(Map<String, CommandIfc>) binds.get(ADMN_CMDS);
+		if (adminCommands.size() > 2) {
+			result = packet.commandResult(Command.DataType.form);
+			Set<String> ids = new LinkedHashSet<String>(adminCommands.keySet());
+			ids.remove(ADD_SCRIPT_CMD);
+			ids.remove(DEL_SCRIPT_CMD);
+			String[] commandIds = ids.toArray(new String[ids.size()]);
+			Command.addFieldValue(result, COMMAND_ID, commandIds[0], "Command Id",
+							commandIds, commandIds);
+		} else {
+			result = packet.commandResult(Command.DataType.result);
+			Command.addTextField(result, "Note",
+							"There is no command script to remove");
+		}
+		return result;
+	}
+
+	@Override
+	public Bindings getBindings() {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+}
