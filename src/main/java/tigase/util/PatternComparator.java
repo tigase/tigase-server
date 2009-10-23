9c19fb9e98cec0f911435ef0ad357e8b4ebe8750
kobit <kobit@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2009-10-23 19:39:50 +0000

    Changes for a new service discovery and scripting API.
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@1895 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

---
+++ src/main/java/tigase/util/PatternComparator.java
@@ -0,0 +1,41 @@
+/*
+ * Tigase Jabber/XMPP Server
+ * Copyright (C) 2004-2008 "Artur Hefczyc" <artur.hefczyc@tigase.org>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://www.gnu.org/licenses/.
+ * 
+ * $Rev$
+ * Last modified by $Author$
+ * $Date$
+ */
+
+package tigase.util;
+
+import java.util.Comparator;
+import java.util.regex.Pattern;
+
+/**
+ * Created: Oct 22, 2009 6:08:50 PM
+ *
+ * @author <a href="mailto:artur.hefczyc@tigase.org">Artur Hefczyc</a>
+ * @version $Rev$
+ */
+public class PatternComparator implements Comparator<Pattern> {
+
+	@Override
+	public int compare(Pattern o1, Pattern o2) {
+		return o1.pattern().compareTo(o2.pattern());
+	}
+
+}
