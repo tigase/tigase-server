781b23201ccc5ce1f7671167a60bb08752c0d8ab
kobit <kobit@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2009-12-19 20:09:36 +0000

    Updated the repository for a new API in the configuration framework.
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@1993 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

--- src/main/java/tigase/conf/ConfigXMLRepository.java
+++ src/main/java/tigase/conf/ConfigXMLRepository.java
@@ -31,6 +31,9 @@ import java.util.Map;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import tigase.annotations.TODO;
 import tigase.db.TigaseDBException;
@@ -50,7 +53,7 @@ import tigase.xml.db.XMLDBException;
  * @version $Rev$
  */
 @TODO(note="Implement access methods for other primitive types.")
-public class ConfigXMLRepository implements ConfigRepositoryIfc {
+public class ConfigXMLRepository extends ConfigurationCache {
 
   public static final String XMPP_CONFIG_FILE_PROPERTY_KEY = "xmpp.config.file";
   public static final String XMPP_CONFIG_FILE_PROPERTY_VAL = "xmpp_server.xml";
@@ -111,6 +114,8 @@ public class ConfigXMLRepository implements ConfigRepositoryIfc {
     def_config = this;
   }
 
+	public ConfigXMLRepository() {	}
+
   private void init() throws XMLDBException {
     try {
       if (new File(config_file).exists()) {
@@ -127,65 +132,134 @@ public class ConfigXMLRepository implements ConfigRepositoryIfc {
   }
 
 	@Override
-	public Map<String, Object> getProperties(String nodeId) {
-		log.config("Reading properties for: " + nodeId);
-		String root = nodeId;
-		String subnode = "";
-		int idx = nodeId.indexOf('/');
-		if (idx >= 0) {
-			root = nodeId.substring(0, idx);
-			subnode = nodeId.substring(idx+1);
-		} // end of if (idx >= 0)
-		log.config("Looking for properties for " + root + " in " + subnode + " node.");
-		List<String> allNodes = new ArrayList<String>();
-		getSubnodes(allNodes, root, subnode);
-		Map<String, Object> props = new TreeMap<String, Object>();
-		String[] keys = getKeys(root, null);
-		log.config("Found keys: " + Arrays.toString(keys));
-		addVals(props, root, null, keys);
-		for (String node : allNodes) {
-			keys = getKeys(root, node);
-			log.config("In node : '" + node + "' found keys: " + Arrays.toString(keys));
-			addVals(props, root, node, keys);
-		} // end of for (String node : allNodes)
-		return props;
-	}
-
-	private void addVals(Map<String, Object> props, String root, String node,
-		String[] keys) {
-		if (keys != null) {
-			for (String key : keys) {
-				String node_tmp = (node == null || node.equals("")) ? "" : node + "/";
-				props.put(node_tmp + key, get(root, node, key, null));
-			} // end of for (String key : keys)
-		} // end of if (keys != null)
+	public void init(Map<String, Object> params)
+			throws ConfigurationException {
+    config_file = (String)params.get("-c");
+		try {
+			init();
+		} catch (XMLDBException ex) {
+			throw new ConfigurationException("Can not initialize configuration XML repository: ", ex);
+		}
 	}
 
 	@Override
-	public void putProperties(String nodeId, Map<String, Object> props) {
-		log.config("Saving properties for: " + nodeId);
-		String root = nodeId;
+	public Set<ConfigItem> getItemsForComponent(String compName) {
+		Set<ConfigItem> result = new LinkedHashSet<ConfigItem>();
+		List<String> allNodes = new ArrayList<String>();
 		String subnode = "";
-		int idx = nodeId.indexOf('/');
-		if (idx >= 0) {
-			root = nodeId.substring(0, idx);
-			subnode = nodeId.substring(idx+1) + "/";
-		} // end of if (idx >= 0)
-		try {	xmldb.addNode1(root);
-		} catch (NodeExistsException e) {	} // end of try-catch
-		for (Map.Entry<String, ?> entry : props.entrySet()) {
-			String node = null;
-			String key = subnode + entry.getKey();
-			idx = key.lastIndexOf('/');
-			if (idx >= 0) {
-				node = key.substring(0, idx);
-				key = key.substring(idx+1);
-			} // end of if (idx >= 0)
-			Object value = entry.getValue();
-			log.config("Setting property: root=" + root + ", node=" + node
-				+ ", key=" + key + ", value=" + value);
-			set(root, node, key, value);
-		} // end of for ()
+		getSubnodes(allNodes, compName, subnode);
+		String[] keys = getKeys(compName, null);
+		log.config("Found keys: " + Arrays.toString(keys));
+		addVals(result, compName, null, keys);
+		for (String node : allNodes) {
+			keys = getKeys(compName, node);
+			log.config("In node : '" + node + "' found keys: " + Arrays.toString(keys));
+			addVals(result, compName, node, keys);
+		} // end of for (String node : allNodes)
+		return result;
+	}
+
+	@Override
+	public ConfigItem getItem(String compName, String node, String key) {
+		try {
+			Object value = xmldb.getData(compName, node, key, null);
+			ConfigItem item = getItemInstance();
+			item.set(compName, node, key, value);
+			return item;
+		} catch (Exception e) {
+			log.log(Level.WARNING, "Can't load value for compName=" + compName
+					+ ", node=" + node + ", key=" + key, e);
+			return null;
+		}
+	}
+
+	@Override
+	public void addItem(String compName, ConfigItem item) {
+		try {
+			xmldb.setData(item.getCompName(), item.getNodeName(), item.getKeyName(), 
+					item.getConfigVal());
+    } catch (NodeNotFoundException e1) {
+      try {
+        xmldb.addNode1(item.getCompName());
+				xmldb.setData(item.getCompName(), item.getNodeName(), item.getKeyName(),
+						item.getConfigVal());
+      } catch (Exception e2) {
+				log.log(Level.WARNING, "Can't add item for compName="
+						+ item.getCompName()
+						+ ", node=" + item.getNodeName() + ", key=" + item.getKeyName()
+						+ ", value=" + item.getConfigValToString(), e2);
+			} // end of try-catch
+		} catch (Exception e) {
+			log.log(Level.WARNING, "Can't add item for compName=" + item.getCompName()
+					+ ", node=" + item.getNodeName() + ", key=" + item.getKeyName()
+					+ ", value=" + item.getConfigValToString(), e);
+		}
+	}
+
+	@Override
+	public void removeItem(String compName, ConfigItem item) {
+		try {
+			xmldb.removeData(item.getCompName(), item.getNodeName(), item.getKeyName());
+		} catch (Exception e) {
+			log.log(Level.WARNING, "Can't remove item for compName=" + item.getCompName()
+					+ ", node=" + item.getNodeName() + ", key=" + item.getKeyName()
+					+ ", value=" + item.getConfigValToString(), e);
+		}
+	}
+
+	@Override
+  public String[] getCompNames() {
+		List<String> comps = xmldb.getAllNode1s();
+		if (comps != null) {
+			return comps.toArray(new String[comps.size()]);
+		} // end of if (comps != null)
+    return null;
+  }
+
+	@Override
+  public String[] getKeys(final String root, final String node) {
+    try {
+      return xmldb.getKeys(root, node);
+    } // end of try
+    catch (NodeNotFoundException e) {
+      return null;
+    } // end of try-catch
+  }
+
+	@Override
+	public int size() {
+		return (int)xmldb.getAllNode1sCount();
+	}
+
+	@Override
+	public Collection<ConfigItem> allItems() throws TigaseDBException {
+		Set<ConfigItem> result = new LinkedHashSet<ConfigItem>();
+		String[] compNames = getCompNames();
+		if (compNames != null) {
+			for (String comp : compNames) {
+				result.addAll(getItemsForComponent(comp));
+			}
+		}
+		return result;
+	}
+
+	/** ************ Old code ************ **/
+
+	private void addVals(Set<ConfigItem> props, String compName, String node,
+		String[] keys) {
+		if (keys != null) {
+			for (String key : keys) {
+				try {
+					Object value = xmldb.getData(compName, node, key, null);
+					ConfigItem item = getItemInstance();
+					item.set(compName, node, key, value);
+					props.add(item);
+				} catch (NodeNotFoundException ex) {
+					log.log(Level.WARNING, "Can't load value for compName=" + compName
+							+ ", node=" + node + ", key=" + key, ex);
+				}
+			} // end of for (String key : keys)
+		} // end of if (keys != null)
 	}
 
 	private void getSubnodes(List<String> result, String root, String node) {
@@ -209,173 +283,6 @@ public class ConfigXMLRepository implements ConfigRepositoryIfc {
 		}
 	}
 
-	@Override
-	public void reload() throws TigaseDBException {
-		
-	}
-
-//	public Object get(final String key) {
-//    return get(key, null);
-//  }
-
-//  public Object get(final String key, final Object def) {
-//    return get(null, key, def);
-//  }
-
-//  public Object get(final String node, final String key, final Object def) {
-//    return get(COMPONENT_NODE, node, key, def);
-//  }
-
-	@Override
-  public Object get(final String root, final String node, final String key,
-    final Object def) {
-    Object result;
-    try {
-      result = xmldb.getData(root, node, key, def);
-    } // end of try
-    catch (NodeNotFoundException e) {
-      result = def;
-    } // end of try-catch
-    return result;
-  }
-
-//  public void set(final String key, final Object value) {
-//    set(null, key, value);
-//  }
-
-//  public void set(final String node, final String key, final Object value) {
-//    set(COMPONENT_NODE, node, key, value);
-//  }
-
-	@Override
-  public void set(final String root, final String node, final String key,
-    final Object value) {
-    try {
-      xmldb.setData(root, node, key, value);
-    } catch (NodeNotFoundException e1) {
-      try {
-        xmldb.addNode1(root);
-        xmldb.setData(root, node, key, value);
-      } catch (Exception e2) { } // end of try-catch
-    } // end of try-catch
-  }
-
-	@Override
-	public void addItem(String key, Object value) throws ConfigurationException {
-		int idx1 = key.indexOf("/");
-		if (idx1 > 0) {
-			String compName = key.substring(0, idx1);
-			int idx2 = key.lastIndexOf("/");
-			String nodeName = null;
-			String keyName = key.substring(idx2 + 1);
-			if (idx1 != idx2) {
-				nodeName = key.substring(idx1 + 1, idx2);
-			}
-			set(compName, nodeName, keyName, value);
-		} else {
-			throw new IllegalArgumentException("You have to provide a key with at least" +
-					" 'component_name/key_name': " + key);
-		}
-	}
-
-//   public void setInt(final String key, final int value) {
-//     setInt(null, key, value);
-//   }
-
-//   public void setInt(final String node, final String key, final int value) {
-//     setInt(COMPONENT_NODE, node, key, value);
-//   }
-
-//   public void setInt(final String root, final String node, final String key,
-//     final int value) {
-//     set(root, node, key, Integer.toString(value));
-//   }
-
-//   public int getInt(final String key, final int def) {
-//     return getInt(null, key, def);
-//   }
-
-//   public int getInt(final String node, final String key, final int def) {
-//     return getInt(COMPONENT_NODE, node, key, def);
-//   }
-
-//   public int getInt(final String root, final String node, final String key,
-//     final int def) {
-//     try {
-//       return get(root, node, key, def);
-//     } // end of try
-//     catch (NumberFormatException e) {
-//       return def;
-//     } // end of try-catch
-//   }
-
-//   public boolean getBoolean(final String node, final String key,
-//     final boolean def) {
-//     return getBoolean(COMPONENT_NODE, node, key, def);
-//   }
-
-//   public boolean getBoolean(final String root, final String node,
-//     final String key, final boolean def) {
-//     final String val = get(root, node, key, Boolean.toString(def));
-//     return (val != null &&
-//       (val.equalsIgnoreCase("yes")
-//         || val.equalsIgnoreCase("true")
-//         || val.equalsIgnoreCase("on")));
-//   }
-
-//   public void setList(final String key, final String[] list) {
-//     setList(null, key, list);
-//   }
-
-//   public void setList(final String node, final String key,
-//     final String[] list) {
-//     setList(COMPONENT_NODE, node, key, list);
-//   }
-
-//   public void setList(final String root, final String node, final String key,
-//     final String[] list) {
-//     try {
-//       xmldb.setData(root, node, key, list);
-//     } // end of try
-//     catch (NodeNotFoundException e1) {
-//       try {
-//         xmldb.addNode1(root);
-//         xmldb.setData(root, node, key, list);
-//       } catch (Exception e2) { } // end of try-catch
-//     } // end of try-catch
-//   }
-
-//   public String[] getList(final String key) {
-//     return getList(null, key);
-//   }
-
-//   public String[] getList(final String node, final String key) {
-//     return getList(COMPONENT_NODE, node, key);
-//   }
-
-//   public String[] getList(final String root, final String node,
-//     final String key) {
-//     try {
-//       return xmldb.getDataList(root, node, key);
-//     } // end of try
-//     catch (NodeNotFoundException e) {
-//       return null;
-//     } // end of try-catch
-//   }
-
-	@Override
-  public String[] getCompNames() {
-		List<String> comps = xmldb.getAllNode1s();
-		if (comps != null) {
-			return comps.toArray(new String[comps.size()]);
-		} // end of if (comps != null)
-    return null;
-  }
-
-//  public String[] getSubnodes(final String node) {
-//    return getSubnodes(COMPONENT_NODE, node);
-//  }
-
   private String[] getSubnodes(final String root, final String node) {
     try {
       return xmldb.getSubnodes(root, node);
@@ -384,128 +291,8 @@ public class ConfigXMLRepository implements ConfigRepositoryIfc {
     } // end of try-catch
   }
 
-//  public String[] getKeys() {
-//    return getKeys(null);
-//  }
-//
-//  public String[] getKeys(final String node) {
-//    return getKeys(COMPONENT_NODE, node);
-//  }
-
-	@Override
-  public String[] getKeys(final String root, final String node) {
-    try {
-      return xmldb.getKeys(root, node);
-    } // end of try
-    catch (NodeNotFoundException e) {
-      return null;
-    } // end of try-catch
-  }
-
-//  public void removeData(final String key) {
-//    removeData(null, key);
-//  }
-//
-//  public void removeData(final String node, final String key) {
-//    removeData(COMPONENT_NODE, node, key);
-//  }
-
-	@Override
-  public void remove(final String root, final String node, final String key) {
-    try {
-      xmldb.removeData(root, node, key);
-    } // end of try
-    catch (NodeNotFoundException e) {
-      log.warning("Attempt to remove data for non-existen node: /"
-        +root+"/"+node);
-    } // end of try-catch
-  }
-
-//  public void removeSubnode(final String node) {
-//    removeSubnode(COMPONENT_NODE, node);
-//  }
-//
-//  public void removeSubnode(final String root, final String node) {
-//    try {
-//      xmldb.removeSubnode(root, node);
-//    } // end of try
-//    catch (NodeNotFoundException e) {
-//      log.warning("Attempt to remove non-existen node: /"
-//        +root+"/"+node);
-//    } // end of try-catch
-//  }
-
   public String nodeForPackage(final Class cls) {
     return cls.getPackage().getName().replace('.', '/');
   }
-
-	@Override
-	public void init(Map<String, Object> params)
-			throws ConfigurationException {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
-
-	@Override
-	public void getDefaults(Map<String, Object> defs,
-			Map<String, Object> params) {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
-
-	@Override
-	public void setProperties(Map<String, Object> properties) {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
-
-	@Override
-	public void removeItem(String key)
-			throws TigaseDBException {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
-
-	@Override
-	public void addItem(ConfigItem item)
-			throws TigaseDBException {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
-
-	@Override
-	public ConfigItem getItem(String key) {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
-
-	@Override
-	public boolean contains(String key) {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
-
-	@Override
-	public int size() {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
-
-	@Override
-	public Collection<ConfigItem> allItems()
-			throws TigaseDBException {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
-
-	@Override
-	public ConfigItem getItemInstance() {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
-
-	@Override
-	public Iterator<ConfigItem> iterator() {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
-
-	@Override
-	public void setDefHostname(String hostname) {
-	}
-
-	@Override
-	public Map<String, Object> getInitProperties() {
-		return null;
-	}
 
 } // ConfigXMLRepository
