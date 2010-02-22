b5095d1a974df2e80f561cf7cb9356159d8302ce
kobit <kobit@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2010-02-22 19:22:36 +0000

    Stopping connection on TLS error.
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@2078 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

--- src/main/java/tigase/server/ext/handlers/StartTLSProcessor.java
+++ src/main/java/tigase/server/ext/handlers/StartTLSProcessor.java
@@ -1,20 +1,20 @@
 /*
  * Tigase Jabber/XMPP Server
  * Copyright (C) 2004-2008 "Artur Hefczyc" <artur.hefczyc@tigase.org>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, version 3 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. Look for COPYING file in the top folder.
  * If not, see http://www.gnu.org/licenses/.
- * 
+ *
  * $Rev$
  * Last modified by $Author$
  * $Date$
@@ -22,17 +22,26 @@
 
 package tigase.server.ext.handlers;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Queue;
-import java.util.logging.Logger;
+//~--- non-JDK imports --------------------------------------------------------
+
 import tigase.server.Packet;
 import tigase.server.ext.ComponentConnection;
 import tigase.server.ext.ComponentProtocolHandler;
 import tigase.server.ext.ExtProcessor;
 import tigase.server.ext.StreamOpenHandler;
+
 import tigase.xml.Element;
+
 import tigase.xmpp.XMPPIOService;
+
+//~--- JDK imports ------------------------------------------------------------
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Queue;
+import java.util.logging.Logger;
+
+//~--- classes ----------------------------------------------------------------
 
 /**
  * Created: Oct 31, 2009 4:54:39 PM
@@ -42,71 +51,38 @@ import tigase.xmpp.XMPPIOService;
  */
 public class StartTLSProcessor implements ExtProcessor {
 
-  /**
-   * Variable <code>log</code> is a class logger.
-   */
-  private static final Logger log =
-    Logger.getLogger(StartTLSProcessor.class.getName());
-
+	/**
+	 * Variable <code>log</code> is a class logger.
+	 */
+	private static final Logger log = Logger.getLogger(StartTLSProcessor.class.getName());
 	private static final String EL_NAME = "starttls";
 	private static final String ID = EL_NAME;
-  private static final Element FEATURES =
-			new Element(EL_NAME,
-			new Element[]{new Element("required")},
-			new String[]{"xmlns"}, new String[]{"urn:ietf:params:xml:ns:xmpp-tls"});
+	private static final Element FEATURES = new Element(EL_NAME,
+		new Element[] { new Element("required") }, new String[] { "xmlns" },
+		new String[] { "urn:ietf:params:xml:ns:xmpp-tls" });
 
+	//~--- get methods ----------------------------------------------------------
+
+	/**
+	 * Method description
+	 *
+	 *
+	 * @return
+	 */
 	@Override
 	public String getId() {
 		return ID;
 	}
 
-	private void initTLS(XMPPIOService<List<ComponentConnection>> serv, String data,
-			boolean client) {
-		try {
-			serv.writeRawData(data);
-			Thread.sleep(10);
-			while (serv.waitingToSend()) {
-				serv.writeRawData(null);
-				Thread.sleep(10);
-			}
-			serv.startTLS(client);
-		} catch (Exception e) {
-
-		}
-	}
-
-	@Override
-	public boolean process(Packet p, XMPPIOService<List<ComponentConnection>> serv,
-			ComponentProtocolHandler handler,
-			Queue<Packet> results) {
-		if (p.getElemName() == EL_NAME) {
-			serv.getSessionData().put(ID, ID);
-			String data = "<proceed xmlns='urn:ietf:params:xml:ns:xmpp-tls'/>";
-			initTLS(serv, data, false);
-			log.fine("Started server side TLS.");
-			return true;
-		}
-		if (p.getElemName() == "proceed") {
-			serv.getSessionData().put(ID, ID);
-			initTLS(serv, null, true);
-			log.fine("Started client side TLS.");
-			StreamOpenHandler soh = handler.getStreamOpenHandler("jabber:client");
-			String data = soh.serviceStarted(serv);
-			serv.xmppStreamOpen(data);
-			log.fine("New stream opened: " + data);
-			return true;
-		}
-		return false;
-	}
-
-	@Override
-	public void startProcessing(Packet p, XMPPIOService<List<ComponentConnection>> serv,
-			ComponentProtocolHandler handler,	Queue<Packet> results) {
-			results.offer(Packet.packetInstance(new Element(EL_NAME,
-					new String[] {"xmlns"},
-					new String[] {"urn:ietf:params:xml:ns:xmpp-tls"}), null, null));
-	}
-
+	/**
+	 * Method description
+	 *
+	 *
+	 * @param serv
+	 * @param handler
+	 *
+	 * @return
+	 */
 	@Override
 	public List<Element> getStreamFeatures(XMPPIOService<List<ComponentConnection>> serv,
 			ComponentProtocolHandler handler) {
@@ -117,4 +93,87 @@ public class StartTLSProcessor implements ExtProcessor {
 		}
 	}
 
+	//~--- methods --------------------------------------------------------------
+
+	/**
+	 * Method description
+	 *
+	 *
+	 * @param p
+	 * @param serv
+	 * @param handler
+	 * @param results
+	 *
+	 * @return
+	 */
+	@Override
+	public boolean process(Packet p, XMPPIOService<List<ComponentConnection>> serv,
+			ComponentProtocolHandler handler, Queue<Packet> results) {
+		if (p.getElemName() == EL_NAME) {
+			serv.getSessionData().put(ID, ID);
+
+			String data = "<proceed xmlns='urn:ietf:params:xml:ns:xmpp-tls'/>";
+
+			initTLS(serv, data, false);
+			log.fine("Started server side TLS.");
+
+			return true;
+		}
+
+		if (p.getElemName() == "proceed") {
+			serv.getSessionData().put(ID, ID);
+			initTLS(serv, null, true);
+			log.fine("Started client side TLS.");
+
+			StreamOpenHandler soh = handler.getStreamOpenHandler("jabber:client");
+			String data = soh.serviceStarted(serv);
+
+			serv.xmppStreamOpen(data);
+			log.fine("New stream opened: " + data);
+
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Method description
+	 *
+	 *
+	 * @param p
+	 * @param serv
+	 * @param handler
+	 * @param results
+	 */
+	@Override
+	public void startProcessing(Packet p, XMPPIOService<List<ComponentConnection>> serv,
+			ComponentProtocolHandler handler, Queue<Packet> results) {
+		results.offer(Packet.packetInstance(new Element(EL_NAME, new String[] { "xmlns" },
+				new String[] { "urn:ietf:params:xml:ns:xmpp-tls" }), null, null));
+	}
+
+	private void initTLS(XMPPIOService<List<ComponentConnection>> serv, String data,
+			boolean client) {
+		try {
+			serv.writeRawData(data);
+			Thread.sleep(10);
+
+			while (serv.waitingToSend()) {
+				serv.writeRawData(null);
+				Thread.sleep(10);
+			}
+
+			serv.startTLS(client);
+		} catch (Exception e) {
+			log.warning("TLS mode start failed: " + e.getMessage());
+			serv.stop();
+		}
+	}
 }
+
+
+//~ Formatted in Sun Code Convention
+
+
+//~ Formatted by Jindent --- http://www.jindent.com
