22d68f114ccc7262a3183a85f7166d4e66a79ec6
kobit <kobit@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2010-03-13 19:31:48 +0000

    Fixed a bug with session bind not being included in stream features.
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@2114 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

--- src/main/java/tigase/xmpp/impl/SessionBind.java
+++ src/main/java/tigase/xmpp/impl/SessionBind.java
@@ -19,20 +19,31 @@
  * Last modified by $Author$
  * $Date$
  */
+
 package tigase.xmpp.impl;
 
-import java.util.Queue;
-import java.util.Map;
-import java.util.logging.Logger;
+//~--- non-JDK imports --------------------------------------------------------
+
+import tigase.db.NonAuthUserRepository;
+
 import tigase.server.Packet;
+
 import tigase.xml.Element;
+
 import tigase.xmpp.Authorization;
 import tigase.xmpp.StanzaType;
+import tigase.xmpp.XMPPException;
 import tigase.xmpp.XMPPProcessor;
 import tigase.xmpp.XMPPProcessorIfc;
 import tigase.xmpp.XMPPResourceConnection;
-import tigase.xmpp.XMPPException;
-import tigase.db.NonAuthUserRepository;
+
+//~--- JDK imports ------------------------------------------------------------
+
+import java.util.Map;
+import java.util.Queue;
+import java.util.logging.Logger;
+
+//~--- classes ----------------------------------------------------------------
 
 /**
  * Describe class SessionBind here.
@@ -43,79 +54,134 @@ import tigase.db.NonAuthUserRepository;
  * @author <a href="mailto:artur.hefczyc@tigase.org">Artur Hefczyc</a>
  * @version $Rev$
  */
-public class SessionBind extends XMPPProcessor
-	implements XMPPProcessorIfc {
-
-  private static final String SESSION_KEY = "Session-Set";
-
-  private static final String XMLNS = "urn:ietf:params:xml:ns:xmpp-session";
-  private static final Logger log =
-		Logger.getLogger("tigase.xmpp.impl.SessionBind");
-
+public class SessionBind extends XMPPProcessor implements XMPPProcessorIfc {
+	private static final String SESSION_KEY = "Session-Set";
+	private static final String XMLNS = "urn:ietf:params:xml:ns:xmpp-session";
+	private static final Logger log = Logger.getLogger("tigase.xmpp.impl.SessionBind");
 	private static final String ID = XMLNS;
-  private static final String[] ELEMENTS = {"session"};
-  private static final String[] XMLNSS = {XMLNS};
-  private static final Element[] FEATURES = {
-		new Element("session", new String[] {"xmlns"}, new String[] {XMLNS})};
-  private static final Element[] DISCO_FEATURES =	{
-		new Element("feature", new String[] {"var"}, new String[] {XMLNS})
-	};
+	private static final String[] ELEMENTS = { "session" };
+	private static final String[] XMLNSS = { XMLNS };
+	private static final Element[] FEATURES = {
+		new Element("session", new String[] { "xmlns" }, new String[] { XMLNS }) };
+	private static final Element[] DISCO_FEATURES = {
+		new Element("feature", new String[] { "var" }, new String[] { XMLNS }) };
+	private static int resGenerator = 0;
 
+	//~--- methods --------------------------------------------------------------
+
+	/**
+	 * Method description
+	 *
+	 *
+	 * @return
+	 */
 	@Override
-  public Element[] supDiscoFeatures(final XMPPResourceConnection session)
-	{ return DISCO_FEATURES; }
-
-
-  private static int resGenerator = 0;
-
-	@Override
-	public String id() { return ID; }
-
-	@Override
-	public String[] supElements()	{ return ELEMENTS; }
-
-	@Override
-	public String[] supNamespaces()	{ return XMLNSS; }
-
-	@Override
-  public Element[] supStreamFeatures(final XMPPResourceConnection session)	{
-    if (session != null &&
-						session.getSessionData(SESSION_KEY) == null &&
-						session.getSessionData(BindResource.RESOURCE_KEY) != null) {
-      return FEATURES;
-    } else {
-      return null;
-    } // end of if (session.isAuthorized()) else
+	public String id() {
+		return ID;
 	}
 
+	/**
+	 * Method description
+	 *
+	 *
+	 * @param packet
+	 * @param session
+	 * @param repo
+	 * @param results
+	 * @param settings
+	 *
+	 * @throws XMPPException
+	 */
 	@Override
-  public void process(final Packet packet, final XMPPResourceConnection session,
-		final NonAuthUserRepository repo, final Queue<Packet> results,
-		final Map<String, Object> settings)
-		throws XMPPException {
-
+	public void process(final Packet packet, final XMPPResourceConnection session,
+			final NonAuthUserRepository repo, final Queue<Packet> results,
+				final Map<String, Object> settings)
+			throws XMPPException {
 		if (session == null) {
 			return;
-		} // end of if (session == null)
+		}    // end of if (session == null)
 
-		if (!session.isAuthorized()) {
-      results.offer(session.getAuthState().getResponseMessage(packet,
-          "Session is not yet authorized.", false));
+		if ( !session.isAuthorized()) {
+			results.offer(session.getAuthState().getResponseMessage(packet,
+					"Session is not yet authorized.", false));
+
 			return;
-		} // end of if (!session.isAuthorized())
+		}    // end of if (!session.isAuthorized())
 
-		//Element request = packet.getElement();
-    StanzaType type = packet.getType();
+		// Element request = packet.getElement();
+		StanzaType type = packet.getType();
+
 		switch (type) {
-		case set:
-			session.putSessionData(SESSION_KEY, "true");
-			results.offer(packet.okResult((String)null, 0));
-			break;
-		default:
-			results.offer(Authorization.BAD_REQUEST.getResponseMessage(packet,
-					"Session type is incorrect", false));
-			break;
-		} // end of switch (type)
+			case set :
+				session.putSessionData(SESSION_KEY, "true");
+				results.offer(packet.okResult((String) null, 0));
+
+				break;
+
+			default :
+				results.offer(Authorization.BAD_REQUEST.getResponseMessage(packet,
+						"Session type is incorrect", false));
+
+				break;
+		}    // end of switch (type)
 	}
 
-} // SessionBind
+	/**
+	 * Method description
+	 *
+	 *
+	 * @param session
+	 *
+	 * @return
+	 */
+	@Override
+	public Element[] supDiscoFeatures(final XMPPResourceConnection session) {
+		return DISCO_FEATURES;
+	}
+
+	/**
+	 * Method description
+	 *
+	 *
+	 * @return
+	 */
+	@Override
+	public String[] supElements() {
+		return ELEMENTS;
+	}
+
+	/**
+	 * Method description
+	 *
+	 *
+	 * @return
+	 */
+	@Override
+	public String[] supNamespaces() {
+		return XMLNSS;
+	}
+
+	/**
+	 * Method description
+	 *
+	 *
+	 * @param session
+	 *
+	 * @return
+	 */
+	@Override
+	public Element[] supStreamFeatures(final XMPPResourceConnection session) {
+		if ((session != null) && (session.getSessionData(SESSION_KEY) == null)
+				&& session.isAuthorized()) {
+			return FEATURES;
+		} else {
+			return null;
+		}    // end of if (session.isAuthorized()) else
+	}
+}    // SessionBind
+
+
+//~ Formatted in Sun Code Convention
+
+
+//~ Formatted by Jindent --- http://www.jindent.com
