c78dc4eadbdf0030c119019d86f1dfa7212aa228
kobit <kobit@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2010-04-16 20:06:06 +0000

    Fixed a problem with ad-hoc commands on multi-virtual host installation
    git-svn-id: file:///home/svn/repos/tigase-server/branches/tigase-server-5.1.0@2187 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

--- src/main/java/tigase/server/AbstractMessageReceiver.java
+++ src/main/java/tigase/server/AbstractMessageReceiver.java
@@ -1133,7 +1133,9 @@ public abstract class AbstractMessageReceiver extends BasicComponent
 								// Maybe this is a command for local processing...
 								boolean processed = false;
 
-								if (packet.isCommand() && getComponentId().equals(packet.getStanzaTo())) {
+								if (packet.isCommand()
+										&& AbstractMessageReceiver.this.getName().equals(packet.getStanzaTo().getLocalpart())
+											&& isLocalDomain(packet.getStanzaTo().getDomain())) {
 									processed = processScriptCommand(packet, results);
 
 									if (processed) {
