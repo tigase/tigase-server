0622505a7963a1bff819a1aab9a3c5c88915b78e
kobit <kobit@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2010-08-30 09:45:10 +0000

    Code formatting changes only.
    git-svn-id: file:///home/svn/repos/tigase-server/branches/tigase-server-5.1.0@2335 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

--- src/main/java/tigase/cluster/strategy/SMNonCachingAllNodes.java
+++ src/main/java/tigase/cluster/strategy/SMNonCachingAllNodes.java
@@ -1,20 +1,20 @@
 /*
  * Tigase Jabber/XMPP Server
  * Copyright (C) 2004-2008 "Artur Hefczyc" <artur.hefczyc@tigase.org>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, version 3 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. Look for COPYING file in the top folder.
  * If not, see http://www.gnu.org/licenses/.
- * 
+ *
  * $Rev$
  * Last modified by $Author$
  * $Date$
@@ -22,16 +22,27 @@
 
 package tigase.cluster.strategy;
 
+//~--- non-JDK imports --------------------------------------------------------
+
+import tigase.cluster.ClusteringStrategyIfc;
+
+import tigase.server.Packet;
+
+import tigase.stats.StatisticsList;
+
+import tigase.xmpp.JID;
+
+//~--- JDK imports ------------------------------------------------------------
+
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.logging.Level;
 import java.util.logging.Logger;
-import tigase.cluster.ClusteringStrategyIfc;
-import tigase.server.Packet;
-import tigase.stats.StatisticsList;
-import tigase.xmpp.JID;
+
+//~--- classes ----------------------------------------------------------------
 
 /**
  * Created: May 13, 2009 9:53:44 AM
@@ -41,82 +52,187 @@ import tigase.xmpp.JID;
  */
 public class SMNonCachingAllNodes implements ClusteringStrategyIfc {
 
-  /**
-   * Variable <code>log</code> is a class logger.
-   */
-  private static final Logger log =
-    Logger.getLogger(SMNonCachingAllNodes.class.getName());
+	/**
+	 * Variable <code>log</code> is a class logger.
+	 */
+	private static final Logger log = Logger.getLogger(SMNonCachingAllNodes.class.getName());
 
-	//private Set<String> cluster_nodes = new ConcurrentSkipListSet<String>();
+	//~--- fields ---------------------------------------------------------------
+
+	// private Set<String> cluster_nodes = new ConcurrentSkipListSet<String>();
 	private List<JID> cl_nodes_list = new CopyOnWriteArrayList<JID>();
-	//private String smName = null;
 
-	@Override
-	public Map<String, Object> getDefaults(Map<String, Object> params) {
-		return null;
-	}
+	// private String smName = null;
 
-	@Override
-	public void setProperties(Map<String, Object> props) {
-	}
+	//~--- methods --------------------------------------------------------------
 
-	@Override
-	public List<JID> getNodesForJid(JID jid) {
-		Collections.rotate(cl_nodes_list, 1);
-		return cl_nodes_list;
-	}
-
-	@Override
-	public void nodeConnected(JID jid) {
-		cl_nodes_list.add(jid);
-		log.fine("Cluster nodes: " + cl_nodes_list.toString());
-	}
-
-	@Override
-	public void nodeDisconnected(JID jid) {
-		cl_nodes_list.remove(jid);
-		log.fine("Cluster nodes: " + cl_nodes_list.toString());
-	}
-
-	@Override
-	public void usersConnected(JID sm, Queue<Packet> results, JID ... jid) {
-	}
-
-	@Override
-	public void userDisconnected(JID sm, Queue<Packet> results, JID jid) {
-	}
-
-	@Override
-	public List<JID> getAllNodes() {
-		return cl_nodes_list;
-	}
-
-	@Override
-	public boolean needsSync() {
-		return false;
-	}
-
-	@Override
-	public void getStatistics(StatisticsList list) {	}
-
-	@Override
-	public boolean hasCompleteJidsInfo() {
-		return false;
-	}
-
+	/**
+	 * Method description
+	 *
+	 *
+	 * @param jid
+	 *
+	 * @return
+	 */
 	@Override
 	public boolean containsJid(JID jid) {
 		return false;
 	}
 
+	//~--- get methods ----------------------------------------------------------
+
+	/**
+	 * Method description
+	 *
+	 *
+	 * @return
+	 */
+	@Override
+	public List<JID> getAllNodes() {
+		return cl_nodes_list;
+	}
+
+	/**
+	 * Method description
+	 *
+	 *
+	 * @param jid
+	 *
+	 * @return
+	 */
 	@Override
 	public JID[] getConnectionIdsForJid(JID jid) {
 		return null;
 	}
 
-//	@Override
-//	public void init(String smName) {
-//		this.smName = smName;
-//	}
+	/**
+	 * Method description
+	 *
+	 *
+	 * @param params
+	 *
+	 * @return
+	 */
+	@Override
+	public Map<String, Object> getDefaults(Map<String, Object> params) {
+		return null;
+	}
 
+	/**
+	 * Method description
+	 *
+	 *
+	 * @param jid
+	 *
+	 * @return
+	 */
+	@Override
+	public List<JID> getNodesForJid(JID jid) {
+		Collections.rotate(cl_nodes_list, 1);
+
+		return cl_nodes_list;
+	}
+
+	/**
+	 * Method description
+	 *
+	 *
+	 * @param list
+	 */
+	@Override
+	public void getStatistics(StatisticsList list) {}
+
+	/**
+	 * Method description
+	 *
+	 *
+	 * @return
+	 */
+	@Override
+	public boolean hasCompleteJidsInfo() {
+		return false;
+	}
+
+	//~--- methods --------------------------------------------------------------
+
+	/**
+	 * Method description
+	 *
+	 *
+	 * @return
+	 */
+	@Override
+	public boolean needsSync() {
+		return false;
+	}
+
+	/**
+	 * Method description
+	 *
+	 *
+	 * @param jid
+	 */
+	@Override
+	public void nodeConnected(JID jid) {
+		cl_nodes_list.add(jid);
+		log.log(Level.FINE, "Cluster nodes: {0}", cl_nodes_list.toString());
+	}
+
+	/**
+	 * Method description
+	 *
+	 *
+	 * @param jid
+	 */
+	@Override
+	public void nodeDisconnected(JID jid) {
+		cl_nodes_list.remove(jid);
+		log.log(Level.FINE, "Cluster nodes: {0}", cl_nodes_list.toString());
+	}
+
+	//~--- set methods ----------------------------------------------------------
+
+	/**
+	 * Method description
+	 *
+	 *
+	 * @param props
+	 */
+	@Override
+	public void setProperties(Map<String, Object> props) {}
+
+	//~--- methods --------------------------------------------------------------
+
+	/**
+	 * Method description
+	 *
+	 *
+	 * @param sm
+	 * @param results
+	 * @param jid
+	 */
+	@Override
+	public void userDisconnected(JID sm, Queue<Packet> results, JID jid) {}
+
+	/**
+	 * Method description
+	 *
+	 *
+	 * @param sm
+	 * @param results
+	 * @param jid
+	 */
+	@Override
+	public void usersConnected(JID sm, Queue<Packet> results, JID... jid) {}
+
+//@Override
+//public void init(String smName) {
+//  this.smName = smName;
+//}
 }
+
+
+//~ Formatted in Sun Code Convention
+
+
+//~ Formatted by Jindent --- http://www.jindent.com
