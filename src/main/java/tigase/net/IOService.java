6a9294339aa46f7f6bd35f51243b8dc9877e98f1
kobit <kobit@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2011-01-25 23:36:41 +0000

    A better locking instead of synchronization for data write and read methods.
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@2504 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

--- src/main/java/tigase/net/IOService.java
+++ src/main/java/tigase/net/IOService.java
@@ -60,6 +60,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -145,8 +146,8 @@ public abstract class IOService<RefObject> implements Callable<IOService<?>>, TL
 	private CharsetEncoder encoder = Charset.forName("UTF-8").newEncoder();
 	private CharsetDecoder decoder = Charset.forName("UTF-8").newDecoder();
 	private CharBuffer cb = CharBuffer.allocate(2048);
-	private final AtomicInteger writeInProgress = new AtomicInteger(0);
-	private final AtomicBoolean readInProgress = new AtomicBoolean(false);
+	private final ReentrantLock writeInProgress = new ReentrantLock();
+	private final ReentrantLock readInProgress = new ReentrantLock();
 
 	//~--- methods --------------------------------------------------------------
 
@@ -210,28 +211,16 @@ public abstract class IOService<RefObject> implements Callable<IOService<?>>, TL
 	 */
 	@Override
 	public IOService<?> call() throws IOException {
+		writeData(null);
 
-		// It is not safe to call below function here....
-		// It might be already executing in different thread...
-		// and we don't want to put any locking or synchronization
-		// processWaitingPackets();
-		// Non-blocking 'kind of' synchronization.
-		// This method is executed for both reading and writing from/to
-		// network socket. It tries to do everything in one go, however
-		// If either reading or writing is in progress then it should skip
-		// the step.
-		if (writeInProgress.compareAndSet(0, 1)) {
-			try {
-				writeData(null);
-			} finally {
-				writeInProgress.decrementAndGet();
-			}
-		}
+		boolean readLock = true;
 
 		if (stopping) {
 			stop();
 		} else {
-			if (readInProgress.compareAndSet(false, true)) {
+			readLock = readInProgress.tryLock();
+
+			if (readLock) {
 				try {
 					processSocketData();
 
@@ -239,12 +228,12 @@ public abstract class IOService<RefObject> implements Callable<IOService<?>>, TL
 						serviceListener.packetsReady(this);
 					}    // end of if (receivedPackets.size() > 0)
 				} finally {
-					readInProgress.set(false);
+					readInProgress.unlock();
 				}
 			}
 		}
 
-		return this;
+		return readLock ? this : null;
 	}
 
 	/**
@@ -826,7 +815,7 @@ public abstract class IOService<RefObject> implements Callable<IOService<?>>, TL
 				// sometimes it happens that the connection has been lost
 				// and the select thinks there are some bytes waiting for reading
 				// and 0 bytes are read
-				if ((++empty_read_call_count) > MAX_ALLOWED_EMPTY_CALLS && (writeInProgress.get() == 0)) {
+				if ((++empty_read_call_count) > MAX_ALLOWED_EMPTY_CALLS && ( !writeInProgress.isLocked())) {
 					log.log(Level.WARNING,
 							"Socket: {0}, Max allowed empty calls excceeded, closing connection.", socketIO);
 					forceStop();
@@ -862,67 +851,81 @@ public abstract class IOService<RefObject> implements Callable<IOService<?>>, TL
 	 * @param data a <code>String</code> value
 	 */
 	protected void writeData(final String data) {
-		writeInProgress.incrementAndGet();
+
+		// Try to lock the data writing method
+		boolean locked = writeInProgress.tryLock();
+
+		// If cannot lock and nothing to send, just leave
+		if ( !locked && (data == null)) {
+			return;
+		}
+
+		// Otherwise wait.....
+		if ( !locked) {
+			writeInProgress.lock();
+		}
 
 		// Avoid concurrent calls here (one from call() and another from
 		// application)
-		synchronized (writeInProgress) {
-			try {
-				if ((data != null) && (data.length() > 0)) {
-					if (log.isLoggable(Level.FINEST)) {
-						if (data.length() < 256) {
-							log.log(Level.FINEST, "Socket: {0}, Writing data ({1}): {2}", new Object[] { socketIO,
-									data.length(), data });
-						} else {
-							log.log(Level.FINEST, "Socket: {0}, Writing data: {1}", new Object[] { socketIO,
-									data.length() });
-						}
-					}
-
-					ByteBuffer dataBuffer = null;
-
-//        int out_buff_size = data.length();
-//        int idx_start = 0;
-//        int idx_offset = Math.min(idx_start + out_buff_size, data.length());
-//
-//        while (idx_start < data.length()) {
-//        String data_str = data.substring(idx_start, idx_offset);
-//        if (log.isLoggable(Level.FINEST)) {
-//        log.finest("Writing data_str (" + data_str.length() + "), idx_start="
-//            + idx_start + ", idx_offset=" + idx_offset + ": " + data_str);
-//        }
-					encoder.reset();
-
-//        dataBuffer = encoder.encode(CharBuffer.wrap(data, idx_start, idx_offset));
-					dataBuffer = encoder.encode(CharBuffer.wrap(data));
-					encoder.flush(dataBuffer);
-					socketIO.write(dataBuffer);
-
-					if (log.isLoggable(Level.FINEST)) {
-						log.log(Level.FINEST, "Socket: {0}, wrote: {1}", new Object[] { socketIO,
+		try {
+			if ((data != null) && (data.length() > 0)) {
+				if (log.isLoggable(Level.FINEST)) {
+					if (data.length() < 256) {
+						log.log(Level.FINEST, "Socket: {0}, Writing data ({1}): {2}", new Object[] { socketIO,
+								data.length(), data });
+					} else {
+						log.log(Level.FINEST, "Socket: {0}, Writing data: {1}", new Object[] { socketIO,
 								data.length() });
 					}
-
-//        idx_start = idx_offset;
-//        idx_offset = Math.min(idx_start + out_buff_size, data.length());
-//          }
-					setLastTransferTime();
-
-					// addWritten(data.length());
-					empty_read_call_count = 0;
-				} else {
-					if (socketIO.waitingToSend()) {
-						socketIO.write(null);
-						setLastTransferTime();
-						empty_read_call_count = 0;
-					}
 				}
-			} catch (Exception e) {
-				forceStop();
-			}
-		}
 
-		writeInProgress.decrementAndGet();
+				ByteBuffer dataBuffer = null;
+
+//      int out_buff_size = data.length();
+//      int idx_start = 0;
+//      int idx_offset = Math.min(idx_start + out_buff_size, data.length());
+//
+//      while (idx_start < data.length()) {
+//      String data_str = data.substring(idx_start, idx_offset);
+//      if (log.isLoggable(Level.FINEST)) {
+//      log.finest("Writing data_str (" + data_str.length() + "), idx_start="
+//          + idx_start + ", idx_offset=" + idx_offset + ": " + data_str);
+//      }
+				encoder.reset();
+
+//      dataBuffer = encoder.encode(CharBuffer.wrap(data, idx_start, idx_offset));
+				dataBuffer = encoder.encode(CharBuffer.wrap(data));
+				encoder.flush(dataBuffer);
+				socketIO.write(dataBuffer);
+
+				if (log.isLoggable(Level.FINEST)) {
+					log.log(Level.FINEST, "Socket: {0}, wrote: {1}",
+							new Object[] { socketIO, data.length() });
+				}
+
+//      idx_start = idx_offset;
+//      idx_offset = Math.min(idx_start + out_buff_size, data.length());
+//        }
+				setLastTransferTime();
+
+				// addWritten(data.length());
+				empty_read_call_count = 0;
+			} else {
+				if (socketIO.waitingToSend()) {
+					socketIO.write(null);
+					setLastTransferTime();
+					empty_read_call_count = 0;
+				}
+			}
+		} catch (Exception e) {
+			if (log.isLoggable(Level.WARNING)) {
+				log.log(Level.WARNING, "Data writing exception", e);
+			}
+
+			forceStop();
+		} finally {
+			writeInProgress.unlock();
+		}
 	}
 
 	private void resizeInputBuffer() throws IOException {
