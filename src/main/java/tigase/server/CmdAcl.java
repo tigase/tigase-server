e6cd215cc9415db3638d88d2413e8feb71f66564
kobit <kobit@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2011-02-24 10:16:37 +0000

    Added JavaDoc comments.
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@2541 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

--- src/main/java/tigase/server/CmdAcl.java
+++ src/main/java/tigase/server/CmdAcl.java
@@ -6,7 +6,32 @@ package tigase.server;
  *
  */
 public enum CmdAcl {
-	ALL, ADMIN, LOCAL, DOMAIN, JID, OTHER;
+	
+	/**
+	 * Everybody can execute the command, even users from a different servers.
+	 */
+	ALL,
+	/**
+	 * Only local server administrators can execute command.
+	 */
+	ADMIN,
+	/**
+	 * Only users who have accounts on this local server can execute the command. 
+	 */
+	LOCAL,
+	/**
+	 * Only users who have an account within the given domain can execute the command.
+	 */
+	DOMAIN,
+	/**
+	 * Comma separated list of JIDs of users who can execute the command. 
+	 */
+	JID,
+	/**
+	 * This is the default. If no access control is provided than by default a list
+	 * of JIDs is assumed.
+	 */
+	OTHER;
 
 	private String aclVal = null;
 
