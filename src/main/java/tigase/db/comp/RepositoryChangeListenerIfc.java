ea30fd0df87bb8ece71667234ae1d569292ffc57
kobit <kobit@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2011-05-24 01:44:57 +0000

    Reconfiguration at run-time for a single configuration option. This is not yet a full reconfiguration support. Rather a simplified implementation which allows to change selected options at run-time. This changes are applied in memory only.
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@2682 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

---
+++ src/main/java/tigase/db/comp/RepositoryChangeListenerIfc.java
@@ -0,0 +1,37 @@
+/*
+* Tigase Jabber/XMPP Server
+* Copyright (C) 2004-2011 "Artur Hefczyc" <artur.hefczyc@tigase.org>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, version 3 of the License.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. Look for COPYING file in the top folder.
+* If not, see http://www.gnu.org/licenses/.
+*
+* $Rev: 2411 $
+* Last modified by $Author: kobit $
+* $Date: 2010-10-27 20:27:58 -0600 (Wed, 27 Oct 2010) $
+ * 
+ */
+package tigase.db.comp;
+
+/**
+ * @author Artur Hefczyc
+ * Created May 23, 2011
+ */
+public interface RepositoryChangeListenerIfc<Item extends RepositoryItem> {
+
+	void itemAdded(Item item);
+	
+	void itemUpdated(Item item);
+
+	void itemRemoved(Item item);
+
+}
