348a7c6c7fae1c7be75a118f1d6280201c4f9cd4
kobit <kobit@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2011-06-04 16:38:02 +0000

    Comments corrections.
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@2692 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

--- src/main/java/tigase/vhosts/VHostManager.java
+++ src/main/java/tigase/vhosts/VHostManager.java
@@ -1,5 +1,5 @@
 /*
- *   Tigase Jabber/XMPP Server
+ *  Tigase Jabber/XMPP Server
  *  Copyright (C) 2004-2008 "Artur Hefczyc" <artur.hefczyc@tigase.org>
  *
  * This program is free software: you can redistribute it and/or modify
