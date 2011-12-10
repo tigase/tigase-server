fd5fb84ece82a887a910c64d012123217e78e917
kobit <kobit@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2011-12-10 03:23:50 +0000

    Ad-hoc command script to run DNS query for a specified domain used to investigate s2s problems.
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@2788 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

---
+++ src/main/groovy/tigase/admin/DNSQuery.groovy
@@ -0,0 +1,77 @@
+/*
+ * Tigase Jabber/XMPP Server
+ * Copyright (C) 2004-2008 "Artur Hefczyc" <artur.hefczyc@tigase.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ * $Rev: $
+ * Last modified by $Author: $
+ * $Date: $
+ */
+
+/*
+ User add script as described in XEP-0133:
+ http://xmpp.org/extensions/xep-0133.html#add-user
+ 
+ AS:Description: DNS Query
+ AS:CommandId: query-dns
+ AS:Component: vhost-man
+ */
+
+package tigase.admin
+
+import tigase.server.*
+import tigase.util.*
+import tigase.xmpp.*
+
+
+def DOMAIN = "domain-name"
+
+def p = (Packet)packet
+
+def domain = Command.getFieldValue(packet, DOMAIN)
+
+if (domain == null) {
+	def result = p.commandResult(Command.DataType.form);
+
+	Command.addTitle(result, "Provide domain name")
+	Command.addInstructions(result, "Fill out this form to query DNS for domain.")
+
+	Command.addFieldValue(result, DOMAIN, domain ?: "", "text-single",
+			"Domain name to query DNS")
+
+	return result
+}
+
+def result = p.commandResult(Command.DataType.result)
+def response_data = []
+try {
+	response_data += "IP: " + DNSResolver.getHostIP(domain)
+	DNSEntry[] entries = DNSResolver.getHostSRV_Entries(domain)
+	int cnt = 1
+	entries.each {
+		response_data += "SRV " + (cnt++) + ": " + it.toString() 
+	}
+	response_data += "Selected SRV: " + DNSResolver.getHostSRV_Entry(domain).toString()
+	response_data += "Selected SRV IP: " + DNSResolver.getHostSRV_IP(domain)
+	
+	
+	Command.addFieldMultiValue(result, "DNS Response: ", response_data);
+} catch (Exception ex) {
+	Command.addTextField(result, "Note", "Problem querying DNS for domain: " + domain);
+	Command.addFieldMultiValue(result, "Exception: ", ex.getStackTrace());
+	
+}
+
+return result
