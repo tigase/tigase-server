f970ca7ab5b3eb0c683a5c5bf4654f5bdeb7ce44
kobit <kobit@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2011-12-20 19:02:48 +0000

    Replaced Handler implementation with Filter which makes much more sense and better uses API.
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@2796 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

---
+++ src/main/java/tigase/util/LogFilter.java
@@ -0,0 +1,52 @@
+/*
+ * Tigase Jabber/XMPP Server
+ * Copyright (C) 2004-2011 "Artur Hefczyc" <artur.hefczyc@tigase.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ * $Rev: 2411 $
+ * Last modified by $Author: kobit $
+ * $Date: 2010-10-27 20:27:58 -0600 (Wed, 27 Oct 2010) $
+ * 
+ */
+package tigase.util;
+
+import java.util.logging.Filter;
+import java.util.logging.LogRecord;
+
+/**
+ * @author kobit Created Dec 20, 2011
+ */
+public class LogFilter implements Filter {
+
+	private String[] trackers = null;
+
+	public LogFilter(String ... trackers) {
+		this.trackers = trackers;
+	}
+
+	@Override
+	public boolean isLoggable(LogRecord record) {
+		boolean matchTracker = false;
+		String msg = record.getMessage();
+		if (msg != null) {
+			int i = 0;
+			while (!matchTracker && i < trackers.length) {
+				matchTracker = msg.contains(trackers[i++]);
+			}
+		}
+		return matchTracker;
+	}
+
+}
