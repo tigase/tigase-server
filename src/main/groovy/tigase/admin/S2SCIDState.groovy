f2880c50514656d89b5ea053d56242954aa31844
kobit <kobit@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2012-04-03 17:10:24 +0000

    A few admin scripts to better diagnose s2s problems.
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@2883 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

---
+++ src/main/groovy/tigase/admin/S2SCIDState.groovy
@@ -0,0 +1,76 @@
+/*
+ * Tigase Jabber/XMPP Server
+ * Copyright (C) 2004-2011 "Artur Hefczyc" <artur.hefczyc@tigase.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ * $Rev: 2411 $
+ * Last modified by $Author: kobit $
+ * $Date: 2010-10-27 20:27:58 -0600 (Wed, 27 Oct 2010) $
+ * 
+ */
+
+/*
+ Returns S2S connection state for given CID
+ AS:Description: S2S get CID connection state
+ AS:CommandId: s2s-get-cid-connection
+ AS:Component: s2s
+ */
+
+package tigase.admin
+
+import tigase.server.*
+import tigase.server.xmppserver.*
+
+
+def CID_KEY = "cid"
+def p = (Packet)packet
+
+def cid = Command.getFieldValue(packet, CID_KEY)
+
+if (cid == null) {
+	def result = p.commandResult(Command.DataType.form);
+
+	Command.addTitle(result, "Get S2S connection state")
+	Command.addInstructions(result, "Fill out this form to get S2S connection state")
+
+	Command.addFieldValue(result, "FORM_TYPE", "http://jabber.org/protocol/admin",
+			"hidden")
+	Command.addFieldValue(result, CID_KEY, cid ?: "", "text-single",
+			"S2S connection CID")
+
+	return result
+}
+
+def conns = []
+def cidConns = (Map)cidConnections
+
+CID c = new CID(cid)
+CIDConnections con = cidConns.get(c)
+
+if (con == null) {
+	conns += "No such CID connection found"
+} else {
+	conns += cid
+	conns += "out in progress: " + con.getOutgoingInProgress()
+	conns += "waiting: " + con.getWaitingCount()
+	conns += "control: " + con.getWaitingControlCount()
+	conns += "incoming: " + con.getIncomingCount()
+	conns += "outgoing: " + con.getOutgoingCount()
+	conns += "out-handshaking: " + con.getOutgoingHandshakingCount()
+}
+
+def result = p.commandResult(Command.DataType.result)
+Command.addFieldMultiValue(result, "S2S connection", conns);
+return result
---
+++ src/main/groovy/tigase/admin/S2SResetBadConnections.groovy
@@ -0,0 +1,56 @@
+/*
+ * Tigase Jabber/XMPP Server
+ * Copyright (C) 2004-2011 "Artur Hefczyc" <artur.hefczyc@tigase.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ * $Rev: 2411 $
+ * Last modified by $Author: kobit $
+ * $Date: 2010-10-27 20:27:58 -0600 (Wed, 27 Oct 2010) $
+ * 
+ */
+
+/*
+ Script tries to find S2S connection in a bad state and resets their state
+ AS:Description: S2S Reset Bad State Connections
+ AS:CommandId: s2s-reset-bad-state-conns
+ AS:Component: s2s
+ */
+
+package tigase.admin
+
+import tigase.server.*
+import tigase.server.xmppserver.*
+
+def p = (Packet)packet
+def cidConns = (Map)cidConnections
+
+def conns = []
+
+conns += "Total count: " + cidConns.size()
+
+cidConns.entrySet().each {
+	CIDConnections con = it.getValue()
+	// Bad state is when the OutgoingInProgress is set to true but there is no outgoing
+	// or outgoing handshaking connections.
+	if (con.getOutgoingInProgress() && (con.getOutgoingCount() == 0) &&
+	(con.getOutgoingHandshakingCount() == 0)) {
+		conns += it.getKey().toString() + ", waiting: " + con.getWaitingCount()
+		con.resetOutgoingInProgress()
+	}
+}
+
+def result = p.commandResult(Command.DataType.result)
+Command.addFieldMultiValue(result, "Reset connections", conns);
+return result
