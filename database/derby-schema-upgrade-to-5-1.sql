e65cca695d04c5d2da3288782e311b2165fe4851
wojtek <wojtek@7d282ba1-3ae6-0310-8f9b-c9008a0864d2>
2012-06-20 18:03:17 +0000

    added procedure creation to update script
    git-svn-id: file:///home/svn/repos/tigase-server/trunk@2931 7d282ba1-3ae6-0310-8f9b-c9008a0864d2

--- database/derby-schema-upgrade-to-5-1.sql
+++ database/derby-schema-upgrade-to-5-1.sql
@@ -20,7 +20,16 @@
 --  $Date: $
 --
 
+
 -- QUERY START:
 call TigPutDBProperty('schema-version','5.1');
+-- QUERY END:
+
+-- QUERY START:
+CREATE procedure TigUpdatePairs(nid bigint, uid bigint, tkey varchar(255), tval varchar(32672)) 
+        PARAMETER STYLE JAVA
+        LANGUAGE JAVA
+        MODIFIES SQL DATA
+        EXTERNAL NAME 'tigase.db.derby.StoredProcedures.tigUpdatePairs';
 -- QUERY END:
 
