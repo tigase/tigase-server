517e7b4d99c747859b42e012a1874ff5b2eb7bed
Andrzej Wójcik <andrzejw@tigase.org>
2013-01-27 00:08:09 +0100

    Added basic support for connecting XMPP clients using WebSocket protocol

---
+++ src/main/java/tigase/server/websocket/WebSocketClientConnectionManager.java
@@ -0,0 +1,54 @@
+/*
+ * Tigase Jabber/XMPP Server
+ * Copyright (C) 2004-2013 "Artur Hefczyc" <artur.hefczyc@tigase.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ * $Rev$
+ * Last modified by $Author$
+ * $Date$
+ */
+package tigase.server.websocket;
+
+import tigase.xmpp.XMPPIOService;
+
+/**
+ * Class implements basic support allowing clients to connect using WebSocket 
+ * protocol
+ * 
+ * @author andrzej
+ */
+public class WebSocketClientConnectionManager extends tigase.server.xmppclient.ClientConnectionManager {
+
+        @Override
+        protected int[] getDefPlainPorts() {
+                return new int[] { 5290 };
+        }
+
+        @Override
+        protected int[] getDefSSLPorts() {
+                return null;
+        }
+
+        /**
+         * Method returns XMPPIOService instance implementing WebSocketXMPPIOService
+         * 
+         * @return 
+         */
+        @Override
+        protected XMPPIOService<Object> getXMPPIOServiceInstance() {
+                return new WebSocketXMPPIOService();
+        }
+
+}
