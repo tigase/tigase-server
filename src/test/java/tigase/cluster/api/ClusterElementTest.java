50a39624b4415bee76d6d7b5b6aa939fd205ddff
Andrzej Wójcik <andrzejw@tigase.org>
2013-02-20 17:25:58 +0100

    Fixed cluster element test

--- src/test/java/tigase/cluster/api/ClusterElementTest.java
+++ src/test/java/tigase/cluster/api/ClusterElementTest.java
@@ -18,13 +18,14 @@ public class ClusterElementTest extends TestCase {
 
         @Test
         public void testGetMethodName() {
+             String[] clusterMethodPath = new String[] { "cluster", "control", "method-call"};
+             
              SimpleParser parser = new SimpleParser();
              DomBuilderHandler handler = new DomBuilderHandler();
              char[] data = "<cluster to=\"sess-man@blue\" type=\"set\" id=\"cl-6627\" xmlns=\"tigase:cluster\" from=\"sess-man@green\"><control><visited-nodes><node-id>sess-man@green</node-id></visited-nodes><method-call name=\"packet-forward-sm-cmd\"/><first-node>sess-man@green</first-node></control><data><presence to=\"test2@test\" xmlns=\"jabber:client\" from=\"test1@test/test\"><status/><priority>5</priority></presence></data></cluster>".toCharArray();
              parser.parse(handler, data, 0, data.length);
              Element elem = handler.getParsedElements().poll();
-             assertEquals("packet-forward-sm-cmd", elem.findChild("/cluster/control/method-call").getAttribute("name"));
-             assertEquals("/cluster/control/method-call", ClusterElement.CLUSTER_METHOD_PATH);
+             assertEquals("packet-forward-sm-cmd", elem.findChild(clusterMethodPath).getAttribute("name"));
              ClusterElement clElem = new ClusterElement(elem);
              assertEquals("packet-forward-sm-cmd", clElem.getMethodName());
         }
