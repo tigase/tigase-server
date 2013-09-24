df42a3b9d012f5e7a6bb0e24f3244613c98f5057
Wojciech Kapcia <wojciech.kapcia@tigase.org>
2013-09-24 21:44:50 +0200

    add hostname parameter to script used to create SQL Server schema

--- scripts/db-create-sqlserver.cmd
+++ scripts/db-create-sqlserver.cmd
@@ -1,21 +1,23 @@
 IF [%1]==[] (
+	set hostname=localhost
 	set database=tigasedb
 	set user=tigase
 	set password=tigase12
 	set root_user=root
 	set root_pass=root
 ) ELSE (
-	set database=%1
-	set user=%2
-	set password=%3
-	set root_user=%4
-	set root_pass=%5
+	set hostname=%1
+	set database=%2
+	set user=%3
+	set password=%4
+	set root_user=%5
+	set root_pass=%6
 )
 
-sqlcmd -U %root_user% -P %root_pass% -Q "CREATE DATABASE [%database%]"
-sqlcmd -U %root_user% -P %root_pass% -Q "CREATE LOGIN [%user%] WITH PASSWORD=N'%password%', DEFAULT_DATABASE=[%database%]"
-sqlcmd -U %root_user% -P %root_pass% -d %database% -Q "ALTER AUTHORIZATION ON DATABASE::%database% TO %user%;"
-sqlcmd -U %root_user% -P %root_pass% -d %database% -i database\sqlserver-schema-5-1-schema.sql
-sqlcmd -U %root_user% -P %root_pass% -d %database% -i database\sqlserver-schema-5-1-sp.sql
-sqlcmd -U %root_user% -P %root_pass% -d %database% -i database\sqlserver-schema-5-1-props.sql
+sqlcmd -H %hostname% -U %root_user% -P %root_pass% -Q "CREATE DATABASE [%database%]"
+sqlcmd -H %hostname% -U %root_user% -P %root_pass% -Q "CREATE LOGIN [%user%] WITH PASSWORD=N'%password%', DEFAULT_DATABASE=[%database%]"
+sqlcmd -H %hostname% -U %root_user% -P %root_pass% -d %database% -Q "ALTER AUTHORIZATION ON DATABASE::%database% TO %user%;"
+sqlcmd -H %hostname% -U %root_user% -P %root_pass% -d %database% -i database\sqlserver-schema-5-1-schema.sql
+sqlcmd -H %hostname% -U %root_user% -P %root_pass% -d %database% -i database\sqlserver-schema-5-1-sp.sql
+sqlcmd -H %hostname% -U %root_user% -P %root_pass% -d %database% -i database\sqlserver-schema-5-1-props.sql
 
