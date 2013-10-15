e1c7d0591674a44d907109830df1629c7e92c618
Artur Hefczyc <artur.hefczyc@tigase.org>
2013-10-15 13:20:52 -0700

    Added getDiscoDescription() method to indicate that this is clustered version running.

--- src/main/java/tigase/cluster/BoshConnectionClustered.java
+++ src/main/java/tigase/cluster/BoshConnectionClustered.java
@@ -1,10 +1,13 @@
 /*
- *   Tigase Jabber/XMPP Server
- *  Copyright (C) 2004-2012 "Artur Hefczyc" <artur.hefczyc@tigase.org>
+ * BoshConnectionClustered.java
+ *
+ * Tigase Jabber/XMPP Server
+ * Copyright (C) 2004-2013 "Tigase, Inc." <office@tigase.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, version 3 of the License.
+ * the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,51 +18,61 @@
  * along with this program. Look for COPYING file in the top folder.
  * If not, see http://www.gnu.org/licenses/.
  *
- * $Rev$
- * Last modified by $Author$
- * $Date$
  */
+
+
 
 package tigase.cluster;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+//~--- non-JDK imports --------------------------------------------------------
+
 import tigase.cluster.api.ClusterControllerIfc;
 import tigase.cluster.api.ClusteredComponentIfc;
-import tigase.server.ServiceChecker;
+
 import tigase.server.bosh.BoshConnectionManager;
+import tigase.server.ServiceChecker;
 import tigase.server.xmppclient.SeeOtherHostIfc;
+
 import tigase.xmpp.BareJID;
 import tigase.xmpp.JID;
 import tigase.xmpp.XMPPIOService;
+
+//~--- JDK imports ------------------------------------------------------------
+
+import java.util.Arrays;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author andrzej
  */
-public class BoshConnectionClustered extends BoshConnectionManager implements
-		ClusteredComponentIfc {
-
+public class BoshConnectionClustered
+				extends BoshConnectionManager
+				implements ClusteredComponentIfc {
 	/**
 	 * Variable <code>log</code> is a class logger.
 	 */
 	private static final Logger log = Logger.getLogger(BoshConnectionClustered.class
 			.getName());
-        
+
+	//~--- fields ---------------------------------------------------------------
+
 	@SuppressWarnings("serial")
 	private List<BareJID> connectedNodes = new CopyOnWriteArrayList<BareJID>() {
 		{
 			add(getDefHostName());
 		}
 	};
-        
+
+	//~--- methods --------------------------------------------------------------
+
 	/**
 	 * Method description
-	 * 
-	 * 
+	 *
+	 *
 	 * @param node
 	 */
 	@Override
@@ -74,18 +87,18 @@ public class BoshConnectionClustered extends BoshConnectionManager implements
 
 				// ugly workaround to sort CopyOnWriteArrayList
 				BareJID[] arr_list = connectedNodes.toArray(new BareJID[connectedNodes.size()]);
+
 				Arrays.sort(arr_list);
 				connectedNodes = new CopyOnWriteArrayList<BareJID>(arr_list);
-
-				if ( see_other_host_strategy != null ){
-					see_other_host_strategy.setNodes( connectedNodes );
+				if (see_other_host_strategy != null) {
+					see_other_host_strategy.setNodes(connectedNodes);
 				}
 			}
 		}
 	}
 
 	/**
-	 * 
+	 *
 	 * @param node
 	 */
 	@Override
@@ -98,11 +111,11 @@ public class BoshConnectionClustered extends BoshConnectionManager implements
 
 		// if (connectedNodes.contains(nodeJID)) {
 		connectedNodes.remove(nodeJID);
-		// }
 
-		if ( see_other_host_strategy != null ){
-			see_other_host_strategy.setNodes( connectedNodes );
-		}	
+		// }
+		if (see_other_host_strategy != null) {
+			see_other_host_strategy.setNodes(connectedNodes);
+		}
 
 		final String hostname = node;
 
@@ -114,30 +127,49 @@ public class BoshConnectionClustered extends BoshConnectionManager implements
 				if (log.isLoggable(Level.FINEST)) {
 					log.log(Level.FINEST, "Checking service for dataReceiver: {0}", dataReceiver);
 				}
-
 				if ((dataReceiver != null) && dataReceiver.getDomain().equals(hostname)) {
 					if (log.isLoggable(Level.FINEST)) {
 						log.finest("Stopping service because corresponding cluster node stopped.");
 					}
-
 					service.stop();
 				}
 			}
 		});
 	}
 
+	//~--- get methods ----------------------------------------------------------
+
+	/**
+	 * Method description
+	 *
+	 *
+	 *
+	 *
+	 * @return a value of <code>String</code>
+	 */
+	@Override
+	public String getDiscoDescription() {
+		return super.getDiscoDescription() + " clustered";
+	}
+
+	/**
+	 * Method description
+	 *
+	 *
+	 * @param see_other_host_class is a <code>String</code>
+	 *
+	 * @return a value of <code>SeeOtherHostIfc</code>
+	 */
 	@Override
 	public SeeOtherHostIfc getSeeOtherHostInstance(String see_other_host_class) {
-		if ( log.isLoggable( Level.FINEST ) ){
-			log.finest( "Configuring see_other_host clustered strategy for: " + see_other_host_class );
+		if (log.isLoggable(Level.FINEST)) {
+			log.finest("Configuring see_other_host clustered strategy for: " +
+					see_other_host_class);
 		}
-
 		if (see_other_host_class == null) {
 			see_other_host_class = SeeOtherHostIfc.CM_SEE_OTHER_HOST_CLASS_PROP_DEF_VAL_CLUSTER;
 		}
-
 		see_other_host_strategy = super.getSeeOtherHostInstance(see_other_host_class);
-
 		if (see_other_host_strategy != null) {
 			see_other_host_strategy.setNodes(connectedNodes);
 		}
@@ -145,15 +177,17 @@ public class BoshConnectionClustered extends BoshConnectionManager implements
 		return see_other_host_strategy;
 	}
 
-	// ~--- set methods ----------------------------------------------------------
+	//~--- set methods ----------------------------------------------------------
 
 	/**
 	 * Method description
-	 * 
-	 * 
+	 *
+	 *
 	 * @param cl_controller
 	 */
-        @Override
-        public void setClusterController(ClusterControllerIfc cl_controller) {
-        }
+	@Override
+	public void setClusterController(ClusterControllerIfc cl_controller) {}
 }
+
+
+//~ Formatted in Tigase Code Convention on 13/10/15
