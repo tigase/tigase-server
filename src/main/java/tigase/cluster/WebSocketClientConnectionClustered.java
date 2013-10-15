091e3d41ba95836e35fbce69be7de5ef6fc8a51f
Artur Hefczyc <artur.hefczyc@tigase.org>
2013-10-15 13:15:14 -0700

    Added getDiscoDescription() method to indicate that this is clustered version running.

--- src/main/java/tigase/cluster/WebSocketClientConnectionClustered.java
+++ src/main/java/tigase/cluster/WebSocketClientConnectionClustered.java
@@ -1,10 +1,13 @@
 /*
- *   Tigase Jabber/XMPP Server
- *  Copyright (C) 2004-2012 "Artur Hefczyc" <artur.hefczyc@tigase.org>
+ * WebSocketClientConnectionClustered.java
+ *
+ * Tigase Jabber/XMPP Server
+ * Copyright (C) 2004-2013 "Tigase, Inc." <office@tigase.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, version 3 of the License.
+ * the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,10 +18,9 @@
  * along with this program. Look for COPYING file in the top folder.
  * If not, see http://www.gnu.org/licenses/.
  *
- * $Rev$
- * Last modified by $Author$
- * $Date$
  */
+
+
 
 package tigase.cluster;
 
@@ -26,58 +28,57 @@ package tigase.cluster;
 
 import tigase.cluster.api.ClusterControllerIfc;
 import tigase.cluster.api.ClusteredComponentIfc;
+
 import tigase.server.ServiceChecker;
 import tigase.server.websocket.WebSocketClientConnectionManager;
 import tigase.server.xmppclient.SeeOtherHostIfc;
 
+import tigase.xmpp.BareJID;
 import tigase.xmpp.JID;
 import tigase.xmpp.XMPPIOService;
-import tigase.xmpp.BareJID;
 
 //~--- JDK imports ------------------------------------------------------------
 
 import java.util.Arrays;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
-
-//~--- classes ----------------------------------------------------------------
 
 /**
  * Describe class WebSocketClientConnectionClustered here.
- * 
- * 
+ *
+ *
  * Created: Sat Jun 21 22:23:18 2008
- * 
+ *
  * @author <a href="mailto:artur.hefczyc@tigase.org">Artur Hefczyc</a>
  * @version $Rev$
  */
-public class WebSocketClientConnectionClustered extends WebSocketClientConnectionManager implements
-		ClusteredComponentIfc {
-
+public class WebSocketClientConnectionClustered
+				extends WebSocketClientConnectionManager
+				implements ClusteredComponentIfc {
 	/**
 	 * Variable <code>log</code> is a class logger.
 	 */
-	private static final Logger log = Logger.getLogger(WebSocketClientConnectionClustered.class
-			.getName());
+	private static final Logger log = Logger.getLogger(
+			WebSocketClientConnectionClustered.class.getName());
+
+	//~--- fields ---------------------------------------------------------------
 
 	private SeeOtherHostIfc see_other_host_strategy = null;
-
 	@SuppressWarnings("serial")
-	private List<BareJID> connectedNodes = new CopyOnWriteArrayList<BareJID>() {
+	private List<BareJID>   connectedNodes          = new CopyOnWriteArrayList<BareJID>() {
 		{
 			add(getDefHostName());
 		}
 	};
 
-	// ~--- methods --------------------------------------------------------------
+	//~--- methods --------------------------------------------------------------
 
 	/**
 	 * Method description
-	 * 
-	 * 
+	 *
+	 *
 	 * @param node
 	 */
 	@Override
@@ -92,16 +93,16 @@ public class WebSocketClientConnectionClustered extends WebSocketClientConnectio
 
 				// ugly workaround to sort CopyOnWriteArrayList
 				BareJID[] arr_list = connectedNodes.toArray(new BareJID[connectedNodes.size()]);
+
 				Arrays.sort(arr_list);
 				connectedNodes = new CopyOnWriteArrayList<BareJID>(arr_list);
-
 				see_other_host_strategy.setNodes(connectedNodes);
 			}
 		}
 	}
 
 	/**
-	 * 
+	 *
 	 * @param node
 	 */
 	@Override
@@ -114,8 +115,8 @@ public class WebSocketClientConnectionClustered extends WebSocketClientConnectio
 
 		// if (connectedNodes.contains(nodeJID)) {
 		connectedNodes.remove(nodeJID);
-		// }
 
+		// }
 		final String hostname = node;
 
 		doForAllServices(new ServiceChecker<XMPPIOService<Object>>() {
@@ -126,30 +127,47 @@ public class WebSocketClientConnectionClustered extends WebSocketClientConnectio
 				if (log.isLoggable(Level.FINEST)) {
 					log.log(Level.FINEST, "Checking service for dataReceiver: {0}", dataReceiver);
 				}
-
 				if ((dataReceiver != null) && dataReceiver.getDomain().equals(hostname)) {
 					if (log.isLoggable(Level.FINEST)) {
 						log.finest("Stopping service because corresponding cluster node stopped.");
 					}
-
 					service.stop();
 				}
 			}
 		});
 	}
 
+	//~--- get methods ----------------------------------------------------------
+
+	/**
+	 * Method description
+	 *
+	 *
+	 * @return a value of <code>String</code>
+	 */
+	@Override
+	public String getDiscoDescription() {
+		return "Websocket connection manager clustered";
+	}
+
+	/**
+	 * Method description
+	 *
+	 *
+	 * @param see_other_host_class is a <code>String</code>
+	 *
+	 * @return a value of <code>SeeOtherHostIfc</code>
+	 */
 	@Override
 	public SeeOtherHostIfc getSeeOtherHostInstance(String see_other_host_class) {
-		if ( log.isLoggable( Level.FINEST ) ){
-			log.finest( "Configuring see_other_host clustered strategy for: " + see_other_host_class );
+		if (log.isLoggable(Level.FINEST)) {
+			log.finest("Configuring see_other_host clustered strategy for: " +
+					see_other_host_class);
 		}
-
 		if (see_other_host_class == null) {
 			see_other_host_class = SeeOtherHostIfc.CM_SEE_OTHER_HOST_CLASS_PROP_DEF_VAL_CLUSTER;
 		}
-
 		see_other_host_strategy = super.getSeeOtherHostInstance(see_other_host_class);
-
 		if (see_other_host_strategy != null) {
 			see_other_host_strategy.setNodes(connectedNodes);
 		}
@@ -157,19 +175,17 @@ public class WebSocketClientConnectionClustered extends WebSocketClientConnectio
 		return see_other_host_strategy;
 	}
 
-	// ~--- set methods ----------------------------------------------------------
+	//~--- set methods ----------------------------------------------------------
 
 	/**
 	 * Method description
-	 * 
-	 * 
+	 *
+	 *
 	 * @param cl_controller
 	 */
 	@Override
-	public void setClusterController(ClusterControllerIfc cl_controller) {
-	}
+	public void setClusterController(ClusterControllerIfc cl_controller) {}
 }
 
-// ~ Formatted in Sun Code Convention
 
-// ~ Formatted by Jindent --- http://www.jindent.com
+//~ Formatted in Tigase Code Convention on 13/10/15
