c04cde99e869be65c9a831acaf6cd129c05f34f0
Wojciech Kapcia <wojciech.kapcia@tigase.org>
2013-10-22 13:40:27 +0200

    Revert "update IzPack to v4.3.4 (fix issue with exception); separate created files from patch"
    This reverts commit b62d91745ce9c08c35c9d5b36c983795cb2a9117.

--- src/main/izpack/java/izpack/izpack/ValidatingConverter.java
+++
@@ -1,8 +0,0 @@
-package com.izforge.izpack.installer;
-
-//does conversion and validates results
-//see Option for result description
-public abstract class ValidatingConverter<F, T> {
-	public abstract Option<T> convert(F from);
-}
-
