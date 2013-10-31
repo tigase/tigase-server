436c66ec9c8e3676b006c1bb90298e687d84d091
Wojciech Kapcia <wojciech.kapcia@tigase.org>
2013-10-31 16:00:27 +0100

    add xml representation of Component Information; related to #1608

--- src/main/java/tigase/server/ComponentInfo.java
+++ src/main/java/tigase/server/ComponentInfo.java
@@ -19,7 +19,10 @@ package tigase.server;
 
 import java.util.HashMap;
 
+import tigase.xml.Element;
+
 /**
+ * Helper class for storing and handling additional informations about components
  *
  * @author Wojciech Kapcia <wojciech.kapcia@tigase.org>
  */
@@ -30,14 +33,38 @@ public class ComponentInfo {
 	private String cls;
 	private HashMap<String, Object> cmpData;
 
+	/**
+	 * Creates ComponentInfo object with initial data
+	 *
+	 * @param cmpTitle title of the component
+	 * @param cmpVersion version of the component
+	 * @param cmpCls class of the component
+	 */
 	public ComponentInfo( String cmpTitle, String cmpVersion, String cmpCls ) {
 		this( null, cmpTitle, cmpVersion, cmpCls);
 	}
 
+	/**
+	 * Creates ComponentInfo object with initial data
+	 *
+	 * @param cmpName name of the component
+	 * @param cmpTitle title of the component
+	 * @param cmpVersion version of the component
+	 * @param cmpCls class of the component
+	 */
 	public ComponentInfo( String cmpName, String cmpTitle, String cmpVersion, String cmpCls ) {
 		this(null, cmpTitle, cmpVersion, cmpCls, new HashMap<String, Object>() );
 	}
 
+	/**
+	 * Creates ComponentInfo object with initial data
+	 *
+	 * @param cmpName name of the component
+	 * @param cmpTitle title of the component
+	 * @param cmpVersion version of the component
+	 * @param cmpCls class of the component
+	 * @param cmpData additional information about component
+	 */
 	public ComponentInfo( String cmpName, String cmpTitle, String cmpVersion, String cmpCls, HashMap<String, Object> cmpData ) {
 		this.name = cmpName;
 		this.title = cmpTitle;
@@ -46,10 +73,21 @@ public class ComponentInfo {
 		this.cmpData = cmpData;
 	}
 
+	/**
+	 * Creates ComponentInfo object with initial data
+	 *
+	 * @param c class of the component
+	 */
 	public ComponentInfo( Class<?> c ) {
 		this( null, c);
 	}
 
+	/**
+	 * Creates ComponentInfo object with initial data
+	 *
+	 * @param cmpName name of the component
+	 * @param c class of the component
+	 */
 	public ComponentInfo(String cmpName, Class<?> c ) {
 		this.name = cmpName;
 		this.title = getImplementationTitle( c );
@@ -58,22 +96,47 @@ public class ComponentInfo {
 		this.cmpData = new HashMap<>();
 	}
 
+	/**
+	 * Allows retrieving of component's name
+	 *
+	 * @return component name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Allows retrieving of component's title
+	 *
+	 * @return component title
+	 */
 	public String getComponentTitle() {
 		return title;
 	}
 
+	/**
+	 * Allows retrieving of component's version
+	 *
+	 * @return component version
+	 */
 	public String getComponentVersion() {
 		return version;
 	}
 
+	/**
+	 * Allows retrieving of component's class
+	 *
+	 * @return component class
+	 */
 	public String getComponentClass() {
 		return cls;
 	}
 
+	/**
+	 * Allows retrieving of component's additional data
+	 *
+	 * @return  component additional data
+	 */
 	public HashMap<String, Object> getComponentData() {
 		return cmpData;
 	}
@@ -87,10 +150,53 @@ public class ComponentInfo {
 					 +  ( cmpData.isEmpty() ? "" : ", componentData=" + cmpData) + '}';
 	}
 
+	/**
+	 * Allows retrieving of component's information as Element
+	 *
+	 * @return component information as Element
+	 */
+	public Element toElement() {
+		Element cmpInfo = new Element( "cmpInfo" );
+		if ( name != null ){
+			cmpInfo.addChild(new Element( "name", name ) );
+		}
+		if ( !title.isEmpty() ){
+			cmpInfo.addChild( new Element( "title", title ) );
+		}
+		if ( !version.isEmpty() ){
+			cmpInfo.addChild( new Element( "version", version ) );
+		}
+		if ( !cls.isEmpty() ){
+			cmpInfo.addChild( new Element( "class", cls ) );
+		}
+		if ( !cmpData.isEmpty() ){
+			Element data = new Element( "cmpData" );
+			for ( String key : cmpData.keySet() ) {
+				data.addChild( new Element( key, cmpData.get( key ).toString() ) );
+			}
+			cmpInfo.addChild( data );
+		}
+		return cmpInfo;
+	}
+
+	/**
+	 * Allows retrieving implementation package (obtained from jar package)
+	 * for a given class
+	 *
+	 * @param c class for which package is to be retrieved
+	 * @return package containing given class
+	 */
 	public static Package getImplementation( Class<?> c ) {
 		return c.getPackage() == null ? XMPPServer.class.getPackage() : c.getPackage();
 	}
 
+	/**
+	 * Allows retrieving implementation version (obtained from jar package)
+	 * for a given class
+	 *
+	 * @param c class for which Package version is to be retrieved
+	 * @return Package version of the given class
+	 */
 	public static String getImplementationVersion( Class<?> c ) {
 		Package p = getImplementation( c );
 
@@ -99,6 +205,13 @@ public class ComponentInfo {
 		return ( version == null ) ? "" : version;
 	}
 
+	/**
+	 * Allows retrieving implementation title (obtained from jar package)
+	 * for a given class
+	 *
+	 * @param c class for which Package title is to be retrieved
+	 * @return Package title of the given class
+	 */
 	public static String getImplementationTitle( Class<?> c ) {
 		Package p = getImplementation( c );
 
@@ -107,6 +220,13 @@ public class ComponentInfo {
 		return title == null ? "" : title;
 	}
 
+	/**
+	 * Allows retrieving implementation information (obtained from jar package)
+	 * for a given class
+	 *
+	 * @param c class for which Package information is to be retrieved
+	 * @return title and version of the Package holding class
+	 */
 	public static String getImplementationInfo( Class<?> c ) {
 		return getImplementationTitle( c ) + ", version: " + getImplementationVersion( c );
 	}
