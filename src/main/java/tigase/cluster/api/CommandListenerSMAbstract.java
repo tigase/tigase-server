f6e7b78def02b8db1028f4466528a799a9c08f3d
Artur Hefczyc <artur.hefczyc@tigase.org>
2013-11-02 19:22:21 -0700

    New clustering API

---
+++ src/main/java/tigase/cluster/api/CommandListenerSMAbstract.java
@@ -0,0 +1,138 @@
+/*
+ * CommandListenerAbstract.java
+ *
+ * Tigase Jabber/XMPP Server
+ * Copyright (C) 2004-2013 "Tigase, Inc." <office@tigase.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+
+
+/*
+* To change this template, choose Tools | Templates
+* and open the template in the editor.
+ */
+package tigase.cluster.api;
+
+//~--- non-JDK imports --------------------------------------------------------
+
+import tigase.stats.StatisticsList;
+
+/**
+ *
+ * @author kobit
+ */
+public abstract class CommandListenerAbstract
+				implements CommandListener {
+	private String commandName;
+
+	//~--- constructors ---------------------------------------------------------
+
+	/**
+	 * Constructs ...
+	 *
+	 *
+	 * @param name
+	 */
+	public CommandListenerAbstract(String name) {
+		setName(name);
+	}
+
+	//~--- methods --------------------------------------------------------------
+
+	/**
+	 * Method description
+	 *
+	 *
+	 * @param cl
+	 *
+	 * 
+	 */
+	@Override
+	public int compareTo(CommandListener cl) {
+		return commandName.compareTo(cl.getName());
+	}
+
+	/**
+	 * Method description
+	 *
+	 *
+	 * @param cl
+	 *
+	 * 
+	 */
+	@Override
+	public boolean equals(Object cl) {
+		return ((cl != null) && (cl instanceof CommandListener) && commandName.equals(
+				((CommandListener) cl).getName()));
+	}
+
+	/**
+	 * Method description
+	 *
+	 *
+	 * 
+	 */
+	@Override
+	public int hashCode() {
+		int hash = 265;
+
+		hash = hash + ((this.commandName != null)
+				? this.commandName.hashCode()
+				: 0);
+
+		return hash;
+	}
+
+	//~--- get methods ----------------------------------------------------------
+
+	/**
+	 * Method description
+	 *
+	 *
+	 * 
+	 */
+	@Override
+	public String getName() {
+		return commandName;
+	}
+
+	/**
+	 * Method description
+	 *
+	 *
+	 * @param list
+	 */
+	@Override
+	public void getStatistics(StatisticsList list) {}
+
+	//~--- set methods ----------------------------------------------------------
+
+	/**
+	 * Method description
+	 *
+	 *
+	 * @param name
+	 */
+	@Override
+	public final void setName(String name) {
+		commandName = name;
+	}
+}
+
+
+//~ Formatted in Tigase Code Convention on 13/07/06
---
+++ src/main/java/tigase/cluster/strategy/cmd/UserPresenceCmd.java
@@ -0,0 +1,147 @@
+/*
+ * UserPresenceCmd.java
+ *
+ * Tigase Jabber/XMPP Server
+ * Copyright (C) 2004-2013 "Tigase, Inc." <office@tigase.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+
+
+/*
+* To change this template, choose Tools | Templates
+* and open the template in the editor.
+ */
+package tigase.cluster.strategy.cmd;
+
+//~--- non-JDK imports --------------------------------------------------------
+
+import tigase.cluster.api.ClusterCommandException;
+import tigase.cluster.api.CommandListenerSMAbstract;
+import tigase.cluster.strategy.ConnectionRecord;
+import tigase.cluster.strategy.DefaultClusteringStrategy;
+
+//import tigase.cluster.strategy.OnlineUsersCachingStrategy;
+import tigase.server.Packet;
+
+import tigase.xml.Element;
+
+import tigase.xmpp.JID;
+import tigase.xmpp.XMPPResourceConnection;
+import tigase.xmpp.XMPPSession;
+
+//~--- JDK imports ------------------------------------------------------------
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
+/**
+ *
+ * @author kobit
+ */
+public class UserPresenceCmd
+				extends CommandListenerSMAbstract<ConnectionRecord,
+						DefaultClusteringStrategy<ConnectionRecord>> {
+	/**
+	 * Variable <code>log</code> is a class logger.
+	 */
+	private static final Logger log = Logger.getLogger(UserPresenceCmd.class.getName());
+
+	//~--- constructors ---------------------------------------------------------
+
+	/**
+	 * Constructs ...
+	 *
+	 *
+	 * @param name
+	 * @param strat
+	 */
+	public UserPresenceCmd(String name, final DefaultClusteringStrategy strat) {
+		super(name, strat);
+	}
+
+	//~--- methods --------------------------------------------------------------
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see tigase.cluster.api.CommandListener#executeCommand(java.util.Map)
+	 */
+
+	/**
+	 * Method description
+	 *
+	 *
+	 * @param fromNode
+	 * @param visitedNodes
+	 * @param data
+	 * @param packets
+	 *
+	 * @throws ClusterCommandException
+	 */
+	@Override
+	public void executeCommand(JID fromNode, Set<JID> visitedNodes, Map<String,
+			String> data, Queue<Element> packets)
+					throws ClusterCommandException {
+		incSyncInTraffic();
+		if (log.isLoggable(Level.FINEST)) {
+			log.log(Level.FINEST,
+					"Called fromNode: {0}, visitedNodes: {1}, data: {2}, packets: {3}",
+					new Object[] { fromNode,
+					visitedNodes, data, packets });
+		}
+
+		ConnectionRecord rec = getConnectionRecord(fromNode, data);
+		XMPPSession      session = getStrategy().getSM().getXMPPSessions().get(rec
+				.getUserJid().getBareJID());
+		Element elem = packets.poll();
+
+		// Update user's XMPPResourceConnection with presence data
+		if (session != null) {
+			XMPPResourceConnection conn = session.getResourceForConnectionId(rec
+					.getConnectionId());
+
+			if (log.isLoggable(Level.FINEST)) {
+				log.log(Level.FINEST,
+						"User's {0} XMPPSession found: {1}, XMPPResourceConnection found: {2}",
+						new Object[] { rec.getUserJid().getBareJID(),
+						session, conn });
+			}
+			if (conn != null) {
+				conn.setPresence(elem);
+			}
+		} else {
+			if (log.isLoggable(Level.FINEST)) {
+				log.log(Level.FINEST,
+						"No user session for presence update: {0}, visitedNodes: {1}, data: {2}, packets: {3}",
+						new Object[] { fromNode,
+						visitedNodes, data, packets });
+			}
+		}
+		if (log.isLoggable(Level.FINEST)) {
+			log.log(Level.FINEST, "User presence jid: {0}, fromNode: {1}", new Object[] { rec
+					.getUserJid(),
+					fromNode });
+		}
+	}
+}
+
+
+//~ Formatted in Tigase Code Convention on 13/11/02
