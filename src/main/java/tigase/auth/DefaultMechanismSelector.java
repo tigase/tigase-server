b6fd37b0490ca193d931af3f960632803fbe262d
Bartosz Malkowski <bmalkowski@tigase.pl>
2014-05-29 14:27:12 +0200

    add configuring SASL mechanisms per domain

--- src/main/java/tigase/auth/DefaultMechanismSelector.java
+++ src/main/java/tigase/auth/DefaultMechanismSelector.java
@@ -1,18 +1,24 @@
 package tigase.auth;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import javax.security.sasl.SaslServerFactory;
 
 import tigase.auth.mechanisms.SaslEXTERNAL;
 import tigase.auth.mechanisms.TigaseSaslServerFactory;
+import tigase.vhosts.VHostItem;
 import tigase.xmpp.XMPPResourceConnection;
 
 public class DefaultMechanismSelector implements MechanismSelector {
+
+	private final Set<String> allowedMechanisms = new HashSet<String>();
 
 	protected Map<String, Object> settings;
 
@@ -24,7 +30,7 @@ public class DefaultMechanismSelector implements MechanismSelector {
 			SaslServerFactory ss = serverFactories.nextElement();
 			String[] x = ss.getMechanismNames(props);
 			for (String name : x) {
-				if (match(ss, name, session))
+				if (match(ss, name, session) && isAllowedForDomain(name, session.getDomain()))
 					result.add(name);
 			}
 		}
@@ -34,6 +40,27 @@ public class DefaultMechanismSelector implements MechanismSelector {
 	@Override
 	public void init(Map<String, Object> settings) {
 		this.settings = settings;
+		String tmp;
+
+		tmp = (String) settings.get("enabled-mechanisms");
+		if (tmp != null) {
+			String[] a = tmp.split(",");
+			if (a != null)
+				allowedMechanisms.addAll(Arrays.asList(a));
+		}
+	}
+
+	protected boolean isAllowedForDomain(final String mechanismName, final VHostItem vhost) {
+		if (vhost.getSaslAllowedMechanisms() != null && vhost.getSaslAllowedMechanisms().length > 0) {
+			for (String allowed : vhost.getSaslAllowedMechanisms()) {
+				if (allowed.equals(mechanismName))
+					return true;
+			}
+			return false;
+		} else if (!allowedMechanisms.isEmpty()) {
+			return allowedMechanisms.contains(mechanismName);
+		}
+		return true;
 	}
 
 	protected boolean match(SaslServerFactory factory, String mechanismName, XMPPResourceConnection session) {
