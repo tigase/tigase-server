f997fe120ddb1c720d1c5bc7123397529354b2c1
Artur Hefczyc <artur.hefczyc@tigase.org>
2014-06-13 12:18:23 -0700

    Updated restart script with uploading config and jar files

--- scripts/restart-all-servers.sh
+++ scripts/restart-all-servers.sh
@@ -9,6 +9,7 @@ fi
 
 DIR="/home/tigase/tigase-server"
 PROP_FILE="cluster.properties"
+JARS="target/tigase-server.jar"
 LOG_TIMESTAMP=`date +"%Y-%m-%d_%H-%M-%S"`
 
 echo -e "=== SERVERS:\n${SERVERS}"
@@ -20,15 +21,24 @@ for s in ${SERVERS} ; do
 
   if [[ ${s} != "#"* ]] ; then
 
-  echo -e "\n\n Copying ${PROP_FILE} file to ${s}"
+    echo -e "\n\n Copying ${PROP_FILE} file to ${s}"
 
-  [[ -f ${PROP_FILE} ]]  && scp ${PROP_FILE} root@${s}:${DIR}/etc/init.properties
+    [[ -f ${PROP_FILE} ]]  && scp ${PROP_FILE} root@${s}:${DIR}/etc/init.properties
 
-	echo -e "\n\n===\trestarting ${s} ==="
+    echo "Copying jar files"
+
+    for f in ${JARS} ; do
+
+      echo -e "\n\n Copying ${f} file to ${s}"
+      [[ -f ${f} ]]  && scp ${f} root@${s}:${DIR}/jars/
+
+    done
+
+	  echo -e "\n\n===\trestarting ${s} ==="
 
     ssh root@${s} "service tigase stop ; sleep 10 ; cp -r ${DIR}/logs ${DIR}/logs_${LOG_TIMESTAMP} ; rm -f ${DIR}/logs/* ; service tigase start"
 
-	echo -e "===\trestart of ${s} FINISHED ==="
+	  echo -e "===\trestart of ${s} FINISHED ==="
 
   fi
 
