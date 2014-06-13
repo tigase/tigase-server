ae212812d745f8706a378067261eec44e69aed71
Artur Hefczyc <artur.hefczyc@tigase.org>
2014-06-13 10:33:46 -0700

    Updated script

--- scripts/restart-all-servers.sh
+++ scripts/restart-all-servers.sh
@@ -8,6 +8,8 @@ else
 fi
 
 DIR="/home/tigase/tigase-server"
+PROP_FILE="cluster.properties"
+LOG_TIMESTAMP=`date +"%Y-%m-%d_%H-%M-%S"`
 
 echo -e "=== SERVERS:\n${SERVERS}"
 echo -e "=== DIR:\n${DIR}"
@@ -18,13 +20,16 @@ for s in ${SERVERS} ; do
 
   if [[ ${s} != "#"* ]] ; then
 
+  echo -e "\n\n Copying ${PROP_FILE} file to ${s}"
+
+  [[ -f ${PROP_FILE} ]]  && scp ${PROP_FILE} root@${s}:${DIR}/etc/init.properties
+
 	echo -e "\n\n===\trestarting ${s} ==="
 
-    ssh root@${s} "service tigase stop ; sleep 10 ; rm -f ${DIR}/logs/* ; service tigase start"
+    ssh root@${s} "service tigase stop ; sleep 10 ; cp -r ${DIR}/logs ${DIR}/logs_${LOG_TIMESTAMP} ; rm -f ${DIR}/logs/* ; service tigase start"
 
 	echo -e "===\trestart of ${s} FINISHED ==="
 
   fi
 
 done
-
