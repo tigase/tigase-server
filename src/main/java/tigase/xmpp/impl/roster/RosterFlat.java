13ddb36c3fb0cad826399044b0f99e80c3bde8e9
Wojciech Kapcia <wojciech.kapcia@tigase.org>
2015-05-28 14:57:07 +0200

    when user modified roster's name to empty or remove the attribute, the response should be name with empty string; it can be enabled with the following option: sess-man/plugins-conf/jabber\:iq\:roster/empty_name_enabled=true and it's disabled by default;
    #2118

--- src/main/java/tigase/xmpp/impl/roster/RosterFlat.java
+++ src/main/java/tigase/xmpp/impl/roster/RosterFlat.java
@@ -151,8 +151,10 @@ public class RosterFlat
 															 relem.getName(), relem.getRosterItem() });
 			}
 		} else {
-			if ((name != null) &&!name.isEmpty()) {
-				relem.setName(name);
+			if ( name != null && !name.isEmpty() ){
+				relem.setName( name );
+			} else if ( emptyNameAllowed ){
+				relem.setName( "" );
 			}
 
 			// Hm, as one user reported this make it impossible to remove the user
