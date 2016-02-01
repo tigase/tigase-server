74db27c19791c186b2eaeeac25dfe2922f2d01ce
Wojciech Kapcia <wojciech.kapcia@tigase.org>
2016-02-01 11:19:07 +0100

    correct mssqlschema; #3228

--- database/sqlserver-schema-7-1-schema.sql
+++ database/sqlserver-schema-7-1-schema.sql
@@ -27,8 +27,8 @@ GO
 -- LOAD FILE: database/sqlserver-schema-5-1-schema.sql
 
 -- QUERY START:
-IF EXISTS(SELECT * FROM sys.indexes WHERE object_id = object_id('schema.tig_pairs') AND NAME ='IX_clustered_tig_pairs_nid_uid_pkey')
-    DROP INDEX IX_clustered_tig_pairs_nid_uid_pkey ON SCHEMA.tig_pairs;
+IF EXISTS(SELECT * FROM sys.indexes WHERE object_id = object_id('dbo.tig_pairs') AND NAME ='IX_clustered_tig_pairs_nid_uid_pkey')
+    DROP INDEX IX_clustered_tig_pairs_nid_uid_pkey ON dbo.tig_pairs;
 -- QUERY END:
 GO
 
