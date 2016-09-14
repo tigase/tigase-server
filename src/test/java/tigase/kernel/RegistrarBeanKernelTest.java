0bfac92b27e691382e4a9f4228e147df70ebab50
Andrzej Wójcik <andrzejw@tigase.org>
2016-09-14 16:17:39 +0200

    Task 4554 - added test to show current issues with TKF and removal of exported or delegated beans

--- src/test/java/tigase/kernel/RegistrarBeanKernelTest.java
+++ src/test/java/tigase/kernel/RegistrarBeanKernelTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import tigase.component.PropertiesBeanConfigurator;
 import tigase.kernel.beans.Bean;
+import tigase.kernel.beans.Inject;
 import tigase.kernel.beans.RegistrarBean;
 import tigase.kernel.core.DependencyGrapher;
 import tigase.kernel.core.Kernel;
@@ -66,6 +67,279 @@ public class RegistrarBeanKernelTest {
 		Assert.assertTrue(exception);
 	}
 
+	@Test
+	public void test02() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+
+		final RegistrarKernel krnl = new RegistrarKernel();
+		krnl.setName("root");
+		krnl.registerBean(PropertiesBeanConfigurator.class).exec();
+		krnl.registerBean(DummyBean.class).exec();
+		krnl.registerBean(RegistrarBeanImplWithLink.class).exec();
+
+		//krnl.startSubKernels();
+
+		DependencyGrapher dg = new DependencyGrapher(krnl);
+		System.out.println(dg.getDependencyGraph());
+
+		Assert.assertNotNull(krnl.getInstance("RegistrarBean"));
+
+		Kernel rb1k = krnl.getInstance("RegistrarBean#KERNEL");
+		Assert.assertNotEquals(null, rb1k.getInstance(Bean1.class));
+		Assert.assertEquals(krnl.getInstance("RegistrarBean"), rb1k.getInstance(RegistrarBeanImplWithLink.class));
+		Assert.assertNotNull(rb1k.getInstance("service"));
+		Assert.assertNotNull(rb1k.getInstance("dummy"));
+		Assert.assertNotNull(rb1k.getInstance("DummyBeanUser"));
+		Assert.assertNotNull(((DummyBeanUser) rb1k.getInstance("DummyBeanUser")).dummyBean);
+
+		krnl.unregister("DummyBean");
+
+		try {
+			// maybe it should still be there but in unresolved state?
+			Assert.assertNull(rb1k.getDependencyManager().getBeanConfig("dummy"));
+			rb1k.getInstance("dummy");
+			Assert.assertTrue(false);
+		} catch (KernelException ex) {
+			// wrong cause of KernelException (caused by NPE), should be thrown due to fact
+			// that "dummy" is DelegatedBeanConfig pointing to removed BeanConfig!
+			Assert.assertTrue(ex.getMessage().contains("Unknown bean"));
+		}
+
+		Assert.assertNull(((DummyBeanUser) rb1k.getInstance("DummyBeanUser")).dummyBean);
+
+		dg = new DependencyGrapher(krnl);
+		System.out.println(dg.getDependencyGraph());
+	}
+
+	@Test
+	public void test03() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+
+		final RegistrarKernel krnl = new RegistrarKernel();
+		krnl.setName("root");
+		krnl.registerBean(PropertiesBeanConfigurator.class).exec();
+		krnl.registerBean(DummyBean2.class).exec();
+		krnl.registerBean(RegistrarBeanImplWithLink2.class).exec();
+
+		//krnl.startSubKernels();
+
+		DependencyGrapher dg = new DependencyGrapher(krnl);
+		System.out.println(dg.getDependencyGraph());
+
+		Assert.assertNotNull(krnl.getInstance("RegistrarBean"));
+
+		Kernel rb1k = krnl.getInstance("RegistrarBean#KERNEL");
+		Assert.assertNotEquals(null, rb1k.getInstance(Bean1.class));
+		Assert.assertEquals(krnl.getInstance("RegistrarBean"), rb1k.getInstance(RegistrarBeanImplWithLink2.class));
+		Assert.assertNotNull(rb1k.getInstance("service"));
+		Assert.assertNotNull(rb1k.getInstance(DummyBean2.class));
+		Assert.assertNotNull(rb1k.getInstance("DummyBean2User"));
+		Assert.assertNotNull(((DummyBean2User) rb1k.getInstance("DummyBean2User")).dummyBean);
+
+		krnl.unregister("DummyBean2");
+
+		try {
+			rb1k.getInstance("DummyBean2");
+			Assert.assertTrue(false);
+		} catch (KernelException ex) {
+			Assert.assertTrue(ex.getMessage().contains("Unknown bean"));
+		}
+
+		Assert.assertNull(((DummyBean2User) rb1k.getInstance("DummyBean2User")).dummyBean);
+
+		dg = new DependencyGrapher(krnl);
+		System.out.println(dg.getDependencyGraph());
+	}
+
+
+	@Test
+	public void test04() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+
+		final RegistrarKernel krnl = new RegistrarKernel();
+		krnl.setName("root");
+		krnl.registerBean(PropertiesBeanConfigurator.class).exec();
+		krnl.registerBean(DummyBean3.class).exec();
+		krnl.registerBean(RegistrarBeanImplWithLink3.class).exec();
+
+		//krnl.startSubKernels();
+
+		DependencyGrapher dg = new DependencyGrapher(krnl);
+		System.out.println(dg.getDependencyGraph());
+
+		Assert.assertNotNull(krnl.getInstance("RegistrarBean"));
+
+		Kernel rb1k = krnl.getInstance("RegistrarBean#KERNEL");
+		Assert.assertNotEquals(null, rb1k.getInstance(Bean1.class));
+		Assert.assertEquals(krnl.getInstance("RegistrarBean"), rb1k.getInstance(RegistrarBeanImplWithLink3.class));
+		Assert.assertNotNull(rb1k.getInstance("service"));
+		Assert.assertNotNull(rb1k.getInstance("dummy"));
+		Assert.assertNotNull(rb1k.getInstance("DummyBean3User"));
+		Assert.assertNotNull(rb1k.getInstance("DummyBean34User"));
+		Assert.assertNotNull(((DummyBean3User) rb1k.getInstance("DummyBean3User")).dummyBean);
+		Assert.assertNotNull(((DummyBean34User) rb1k.getInstance("DummyBean34User")).dummyBean3User);
+
+		krnl.unregister("DummyBean");
+
+		try {
+			// maybe it should still be there but in unresolved state?
+			Assert.assertNull(rb1k.getDependencyManager().getBeanConfig("dummy"));
+			rb1k.getInstance("dummy");
+			Assert.assertTrue(false);
+		} catch (KernelException ex) {
+			// wrong cause of KernelException (caused by NPE), should be thrown due to fact
+			// that "dummy" is DelegatedBeanConfig pointing to removed BeanConfig!
+			Assert.assertTrue(ex.getMessage().contains("Unknown bean"));
+		}
+
+		try {
+			rb1k.getInstance("DummyBean3User");
+			Assert.assertTrue(false);
+		} catch (KernelException ex) {
+			Assert.assertTrue(true);
+		}
+
+		Assert.assertNull(((DummyBean34User) rb1k.getInstance("DummyBean34User")).dummyBean3User);
+
+		krnl.registerBean(DummyBean3.class).exec();
+
+		Assert.assertNotNull(((DummyBean34User) rb1k.getInstance("DummyBean34User")).dummyBean3User);
+		Assert.assertNotNull(((DummyBean3User) rb1k.getInstance("DummyBean3User")).dummyBean);
+
+		dg = new DependencyGrapher(krnl);
+		System.out.println(dg.getDependencyGraph());
+	}
+
+	@Test
+	public void test05() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+
+		final RegistrarKernel krnl = new RegistrarKernel();
+		krnl.setName("root");
+		krnl.registerBean(PropertiesBeanConfigurator.class).exec();
+		krnl.registerBean(DummyBean4.class).exec();
+		krnl.registerBean(RegistrarBeanImplWithLink4.class).exec();
+
+		//krnl.startSubKernels();
+
+		DependencyGrapher dg = new DependencyGrapher(krnl);
+		System.out.println(dg.getDependencyGraph());
+
+		Assert.assertNotNull(krnl.getInstance("RegistrarBean"));
+
+		Kernel rb1k = krnl.getInstance("RegistrarBean#KERNEL");
+		Assert.assertNotEquals(null, rb1k.getInstance(Bean1.class));
+		Assert.assertEquals(krnl.getInstance("RegistrarBean"), rb1k.getInstance(RegistrarBeanImplWithLink4.class));
+		Assert.assertNotNull(rb1k.getInstance("service"));
+		Assert.assertNotNull(rb1k.getInstance(DummyBean4.class));
+		Assert.assertNotNull(rb1k.getInstance("DummyBean4User"));
+		Assert.assertNotNull(rb1k.getInstance("DummyBean34User"));
+		Assert.assertNotNull(((DummyBean4User) rb1k.getInstance("DummyBean4User")).dummyBean);
+		Assert.assertNotNull(((DummyBean34User) rb1k.getInstance("DummyBean34User")).dummyBean4User);
+
+		krnl.unregister("DummyBean4");
+
+		try {
+			rb1k.getInstance("DummyBean4");
+			Assert.assertTrue(false);
+		} catch (KernelException ex) {
+			Assert.assertTrue(ex.getMessage().contains("Unknown bean"));
+		}
+
+		try {
+			rb1k.getInstance("DummyBean4User");
+			Assert.assertTrue(false);
+		} catch (KernelException ex) {
+			Assert.assertTrue(true);
+		}
+
+		Assert.assertNull(((DummyBean34User) rb1k.getInstance("DummyBean34User")).dummyBean4User);
+
+		krnl.registerBean(DummyBean4.class).exec();
+
+		Assert.assertNotNull(((DummyBean34User) rb1k.getInstance("DummyBean34User")).dummyBean4User);
+		Assert.assertNotNull(((DummyBean3User) rb1k.getInstance("DummyBean3User")).dummyBean);
+
+
+		dg = new DependencyGrapher(krnl);
+		System.out.println(dg.getDependencyGraph());
+	}
+
+	@Bean(name="DummyBean")
+	public static class DummyBean {
+
+		public DummyBean() {}
+
+	}
+
+	@Bean(name="DummyBeanUser")
+	public static class DummyBeanUser {
+
+		@Inject(nullAllowed = true)
+		public DummyBean dummyBean;
+
+		public DummyBeanUser() {}
+
+	}
+
+	@Bean(name="DummyBean2", exportable = true)
+	public static class DummyBean2 {
+
+		public DummyBean2() {}
+
+	}
+
+	@Bean(name="DummyBean2User")
+	public static class DummyBean2User {
+
+		@Inject(nullAllowed = true)
+		public DummyBean2 dummyBean;
+
+		public DummyBean2User() {}
+
+	}
+
+	@Bean(name="DummyBean3")
+	public static class DummyBean3 {
+
+		public DummyBean3() {}
+
+	}
+
+	@Bean(name="DummyBean3User")
+	public static class DummyBean3User {
+
+		@Inject
+		public DummyBean3 dummyBean;
+
+		public DummyBean3User() {}
+
+	}
+
+	@Bean(name="DummyBean4", exportable = true)
+	public static class DummyBean4 {
+
+		public DummyBean4() {}
+
+	}
+
+	@Bean(name="DummyBean4User")
+	public static class DummyBean4User {
+
+		@Inject
+		public DummyBean4 dummyBean;
+
+		public DummyBean4User() {}
+
+	}
+
+	@Bean(name="DummyBean34User")
+	public static class DummyBean34User {
+
+		@Inject(nullAllowed = true)
+		public DummyBean3User dummyBean3User;
+
+		@Inject(nullAllowed = true)
+		public DummyBean4User dummyBean4User;
+
+	}
+
 	@Bean(name="RegistrarBean")
 	public static class RegistrarBeanImpl implements RegistrarBean {
 
@@ -84,4 +358,90 @@ public class RegistrarBeanKernelTest {
 			kernel.unregister("bean1");
 		}
 	}
+
+	@Bean(name="RegistrarBean")
+	public static class RegistrarBeanImplWithLink implements RegistrarBean {
+
+
+		public RegistrarBeanImplWithLink() {
+
+		}
+
+		@Override
+		public void register(Kernel kernel) {
+			kernel.getParent().ln("DummyBean", kernel, "dummy");
+			kernel.registerBean(DummyBeanUser.class).exec();
+			kernel.registerBean(Bean1.class).exec();
+		}
+
+		@Override
+		public void unregister(Kernel kernel) {
+			kernel.unregister("bean1");
+		}
+	}
+
+
+	@Bean(name="RegistrarBean")
+	public static class RegistrarBeanImplWithLink2 implements RegistrarBean {
+
+
+		public RegistrarBeanImplWithLink2() {
+
+		}
+
+		@Override
+		public void register(Kernel kernel) {
+			kernel.registerBean(DummyBean2User.class).exec();
+			kernel.registerBean(Bean1.class).exec();
+		}
+
+		@Override
+		public void unregister(Kernel kernel) {
+			kernel.unregister("bean1");
+		}
+	}
+
+	@Bean(name="RegistrarBean")
+	public static class RegistrarBeanImplWithLink3 implements RegistrarBean {
+
+
+		public RegistrarBeanImplWithLink3() {
+
+		}
+
+		@Override
+		public void register(Kernel kernel) {
+			kernel.getParent().ln("DummyBean3", kernel, "dummy");
+			kernel.registerBean(DummyBean3User.class).exec();
+			kernel.registerBean(DummyBean34User.class).exec();
+			kernel.registerBean(Bean1.class).exec();
+		}
+
+		@Override
+		public void unregister(Kernel kernel) {
+			kernel.unregister("bean1");
+		}
+	}
+
+	@Bean(name="RegistrarBean")
+	public static class RegistrarBeanImplWithLink4 implements RegistrarBean {
+
+
+		public RegistrarBeanImplWithLink4() {
+
+		}
+
+		@Override
+		public void register(Kernel kernel) {
+			kernel.registerBean(DummyBean4User.class).exec();
+			kernel.registerBean(DummyBean34User.class).exec();
+			kernel.registerBean(Bean1.class).exec();
+		}
+
+		@Override
+		public void unregister(Kernel kernel) {
+			kernel.unregister("bean1");
+		}
+	}
+
 }
