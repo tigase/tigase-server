c8a9f64ed340c1af97cee33438608dd01bf9d8ad
Andrzej Wójcik <andrzejw@tigase.org>
2017-02-15 12:08:00 +0100

    Task #4833 - deprecating XMPPServer.getConfigurator() method as it will be removed in 7.2.0

--- src/main/java/tigase/server/XMPPServer.java
+++ src/main/java/tigase/server/XMPPServer.java
@@ -178,6 +178,7 @@ public final class XMPPServer {
 	 * @return implementation of {@link tigase.conf.ConfiguratorAbstract}
 	 *         interface.
 	 */
+	@Deprecated
 	public static ConfiguratorAbstract getConfigurator() {
 		return config;
 	}
