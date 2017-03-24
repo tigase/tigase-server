038751fe5ac5ed549069a0e3231edc347097da5a
Andrzej Wójcik <andrzejw@tigase.org>
2017-03-24 20:20:52 +0100

    #5033: added support for storing emoji in tig_pairs in pval

--- server/database/mysql-schema-7-2-sp.sql
+++ server/database/mysql-schema-7-2-sp.sql
@@ -42,6 +42,28 @@ end //
 delimiter ;
 
 -- QUERY START:
+drop procedure if exists TigUpdatePairs;
+-- QUERY END:
+
+delimiter //
+
+-- QUERY START:
+-- Procedure to efficiently and safely update data in tig_pairs table
+create procedure TigUpdatePairs(_nid bigint, _uid bigint, _tkey varchar(255) CHARSET utf8, _tval mediumtext CHARSET utf8mb4)
+begin
+  if exists(SELECT 1 FROM tig_pairs WHERE nid = _nid AND uid = _uid AND pkey = _tkey)
+  then
+    UPDATE tig_pairs SET pval = _tval WHERE nid = _nid AND uid = _uid AND pkey = _tkey;
+  ELSE
+    INSERT INTO tig_pairs (nid, uid, pkey, pval) VALUES (_nid, _uid, _tkey, _tval);
+  END IF;
+end //
+-- QUERY END:
+
+
+delimiter ;
+
+-- QUERY START:
 drop procedure if exists Tig_OfflineMessages_AddMessage;
 -- QUERY END:
 -- QUERY START:
