tigase/tigase-server

commit aaa9a7f110e8b78e5f49bcd9b360058f6ec2ee7d

#6000 fix problem with subscription from dynamic roster wasn't correctly recognised while broadcasting presence

src/main/java/tigase/xmpp/impl/Presence.java
@@ -237,7 +237,7 @@ public class Presence
 		JID[] buddies = roster_util.getBuddies(session, SUB_BOTH);
 
 		try {
-			buddies = DynamicRoster.addBuddies(session, settings, buddies);
+			buddies = DynamicRoster.addBuddies(session, settings, buddies, TO_SUBSCRIBED);
 		} catch (RosterRetrievingException | RepositoryAccessException ex) {
 
 			// Ignore, handled in the JabberIqRoster code
@@ -789,7 +789,7 @@ public class Presence
 		JID[] buddies = roster.getBuddies(session, subscrs);
 
 		try {
-			buddies = DynamicRoster.addBuddies(session, settings, buddies);
+			buddies = DynamicRoster.addBuddies(session, settings, buddies, subscrs);
 		} catch (RosterRetrievingException | RepositoryAccessException ex) {
 
 			// Ignore, handled in the JabberIqRoster code
@@ -1706,12 +1706,10 @@ public class Presence
 
 						// Resend pending in subscription requests
 						resendPendingInRequests(session, results);
-					} else {
-
-						// Broadcast initial presence to 'from' or 'both' contacts
-						sendPresenceBroadcast(StanzaType.available, session, FROM_SUBSCRIBED,
-															 results, presenceEl, settings, roster_util);
 					}
+					// Broadcast initial presence to 'from' or 'both' contacts
+					sendPresenceBroadcast(StanzaType.available, session, FROM_SUBSCRIBED,
+														 results, presenceEl, settings, roster_util);
 
 					// Broadcast initial presence to other available user resources
 					updateUserResources(presenceEl, session, results, first);

src/main/java/tigase/xmpp/impl/PresenceAbstract.java
@@ -42,9 +42,7 @@ import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import static tigase.xmpp.impl.roster.RosterAbstract.SUB_BOTH;
-import static tigase.xmpp.impl.roster.RosterAbstract.SUB_FROM;
-import static tigase.xmpp.impl.roster.RosterAbstract.SUB_TO;
+import static tigase.xmpp.impl.roster.RosterAbstract.*;
 
 /**
  * Class responsible for handling Presence packets
@@ -116,7 +114,7 @@ public abstract class PresenceAbstract
 		JID[] buddies = roster_util.getBuddies(session, SUB_BOTH);
 
 		try {
-			buddies = DynamicRoster.addBuddies(session, settings, buddies);
+			buddies = DynamicRoster.addBuddies(session, settings, buddies, TO_SUBSCRIBED);
 		} catch (RosterRetrievingException | RepositoryAccessException ex) {
 
 			// Ignore, handled in the JabberIqRoster code

src/main/java/tigase/xmpp/impl/PresenceState.java
@@ -840,12 +840,10 @@ public class PresenceState extends PresenceAbstract implements XMPPStopListenerI
 
 						// Resend pending in subscription requests
 						resendPendingInRequests(session, results);
-					} else {
-
-						// Broadcast initial presence to 'from' or 'both' contacts
-						sendPresenceBroadcast(StanzaType.available, session, FROM_SUBSCRIBED,
-															 results, presenceEl, settings, roster_util);
 					}
+					// Broadcast initial presence to 'from' or 'both' contacts
+					sendPresenceBroadcast(StanzaType.available, session, FROM_SUBSCRIBED,
+														 results, presenceEl, settings, roster_util);
 
 					// Broadcast initial presence to other available user resources
 					updateUserResources(presenceEl, session, results, first);
@@ -947,7 +945,7 @@ public class PresenceState extends PresenceAbstract implements XMPPStopListenerI
 		JID[] buddies = roster.getBuddies(session, subscrs);
 
 		try {
-			buddies = DynamicRoster.addBuddies(session, settings, buddies);
+			buddies = DynamicRoster.addBuddies(session, settings, buddies, subscrs);
 		} catch (RosterRetrievingException | RepositoryAccessException ex) {
 
 			// Ignore, handled in the JabberIqRoster code

src/main/java/tigase/xmpp/impl/roster/DynamicRoster.java
@@ -27,18 +27,17 @@ package tigase.xmpp.impl.roster;
 //~--- non-JDK imports --------------------------------------------------------
 
 import tigase.xml.Element;
-
 import tigase.xmpp.JID;
 import tigase.xmpp.NotAuthorizedException;
 import tigase.xmpp.XMPPResourceConnection;
 
-//~--- JDK imports ------------------------------------------------------------
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.Map;
+
+import static tigase.xmpp.impl.roster.RosterAbstract.SubscriptionType;
+
+//~--- JDK imports ------------------------------------------------------------
 
 /**
  * Describe class DynamicRoster here.
@@ -52,6 +51,8 @@ import java.util.Map;
 public abstract class DynamicRoster {
 	private static final String DYNAMIC_ROSTERS         = "dynamic-rosters";
 	private static final String DYNAMIC_ROSTERS_CLASSES = "dynamic-roster-classes";
+
+	private static final EnumSet<SubscriptionType> subs = EnumSet.noneOf(SubscriptionType.class);
 
 	/**
 	 * Private logger for class instances.
@@ -80,15 +81,39 @@ public abstract class DynamicRoster {
 			Object> settings, JID[] buddies)
 					throws NotAuthorizedException, RosterRetrievingException,
 							RepositoryAccessException {
+		return addBuddies(session,settings,buddies, subs);
+	}
+
+	public static JID[] addBuddies(final XMPPResourceConnection session, final Map<String,
+			Object> settings, JID[] buddies, final EnumSet<RosterAbstract.SubscriptionType> subscrs)
+					throws NotAuthorizedException, RosterRetrievingException,
+							RepositoryAccessException {
 		List<JID> result = getBuddiesList(session, settings);
+
+		if (result != null && subscrs != null && !subscrs.isEmpty()) {
+			final Iterator<JID> iterator = result.iterator();
+			while (iterator.hasNext()) {
+				final JID jid = iterator.next();
+				Element buddy = getBuddyItem(session, settings, jid);
+				String sub = null;
+				if (buddy != null && ((sub = buddy.getAttributeStaticStr("subscription")) != null)) {
+					try {
+						final SubscriptionType subType = SubscriptionType.valueOf(sub.toLowerCase());
+						if (!subscrs.contains(subType)) {
+							iterator.remove();
+						}
+					} catch (IllegalArgumentException e) {
+						log.log(Level.FINEST, "Illegal dynamic roster element, skipping");
+					}
+				}
+			}
+		}
 
 		if (buddies != null) {
 			if (result == null) {
 				result = new ArrayList<JID>();
 			}
 			addBuddiesToList(result, buddies);
-
-			// result.addAll(Arrays.asList(buddies));
 		}
 		if ((result != null) && (result.size() > 0)) {
 			return result.toArray(new JID[result.size()]);

src/main/java/tigase/xmpp/impl/roster/DynamicRosterTest123.java
@@ -37,6 +37,7 @@ import tigase.xmpp.XMPPResourceConnection;
 public class DynamicRosterTest123 implements DynamicRosterIfc {
 
 	private static final String[] buddy_names = { "test1", "test2", "test3" };
+	private static final String[] managers = { "manager1" };
 
 	@Override
 	public void setItemExtraData(Element item) {
@@ -71,6 +72,12 @@ public class DynamicRosterTest123 implements DynamicRosterIfc {
 				result.add(JID.jidInstanceNS(name, domain, null));
 			}
 		}
+		for (String name : managers) {
+			if (!name.equals(session.getUserName())) {
+				result.add(JID.jidInstanceNS(name, domain, null));
+			}
+		}
+
 		return result.toArray(new JID[result.size()]);
 	}
 
@@ -79,7 +86,11 @@ public class DynamicRosterTest123 implements DynamicRosterIfc {
 			throws NotAuthorizedException {
 		return new Element("item", new Element[] { new Element("group", "test group") },
 				new String[] { "jid", "name", "subscription" }, new String[] {
-						buddy.getBareJID().toString(), buddy.getLocalpart(), "both" });
+					buddy.getBareJID().toString(),
+					buddy.getLocalpart(),
+					( buddy.toString().contains("manager") ? "from" : "to" )
+		});
+
 	}
 
 	@Override

commit c6c75bf01b4faaf845011bb663feaa9eec8474ba

#6000 fix issue with multiple presence being generated (introduced in aaa9a7f110e8b78e5f49bcd9b360058f6ec2ee7d)

src/main/java/tigase/xmpp/impl/Presence.java
@@ -1706,10 +1706,11 @@ public class Presence
 
 						// Resend pending in subscription requests
 						resendPendingInRequests(session, results);
+					} else {
+						// Broadcast initial presence to 'from' or 'both' contacts
+						sendPresenceBroadcast(StanzaType.available, session, FROM_SUBSCRIBED, results, presenceEl,
+						                      settings, roster_util);
 					}
-					// Broadcast initial presence to 'from' or 'both' contacts
-					sendPresenceBroadcast(StanzaType.available, session, FROM_SUBSCRIBED,
-														 results, presenceEl, settings, roster_util);
 
 					// Broadcast initial presence to other available user resources
 					updateUserResources(presenceEl, session, results, first);

src/main/java/tigase/xmpp/impl/PresenceAbstract.java
@@ -114,7 +114,7 @@ public abstract class PresenceAbstract
 		JID[] buddies = roster_util.getBuddies(session, SUB_BOTH);
 
 		try {
-			buddies = DynamicRoster.addBuddies(session, settings, buddies, TO_SUBSCRIBED);
+			buddies = DynamicRoster.addBuddies(session, settings, buddies, SUB_BOTH);
 		} catch (RosterRetrievingException | RepositoryAccessException ex) {
 
 			// Ignore, handled in the JabberIqRoster code
@@ -144,6 +144,12 @@ public abstract class PresenceAbstract
 
 		JID[] buddies_to = roster_util.getBuddies(session, SUB_TO);
 
+		try {
+			buddies_to = DynamicRoster.addBuddies(session, settings, buddies_to, SUB_TO);
+		} catch (RosterRetrievingException | RepositoryAccessException ex) {
+			// Ignore, handled in the JabberIqRoster code
+		}
+
 		if (buddies_to != null) {
 			for (JID buddy : buddies_to) {
 				if (requiresPresenceSending(roster_util, buddy, session, true)) {
@@ -163,6 +169,12 @@ public abstract class PresenceAbstract
 		// TODO: It might be a marginal number of cases here but just make it clear
 		// we send a presence here regardless
 		JID[] buddies_from = roster_util.getBuddies(session, SUB_FROM);
+
+		try {
+			buddies_from = DynamicRoster.addBuddies(session, settings, buddies_to, SUB_FROM);
+		} catch (RosterRetrievingException | RepositoryAccessException ex) {
+			// Ignore, handled in the JabberIqRoster code
+		}
 
 		if (buddies_from != null) {
 			for (JID buddy : buddies_from) {

src/main/java/tigase/xmpp/impl/PresenceState.java
@@ -840,10 +840,11 @@ public class PresenceState extends PresenceAbstract implements XMPPStopListenerI
 
 						// Resend pending in subscription requests
 						resendPendingInRequests(session, results);
+					} else {
+						// Broadcast initial presence to 'from' or 'both' contacts
+						sendPresenceBroadcast(StanzaType.available, session, FROM_SUBSCRIBED,
+															 results, presenceEl, settings, roster_util);
 					}
-					// Broadcast initial presence to 'from' or 'both' contacts
-					sendPresenceBroadcast(StanzaType.available, session, FROM_SUBSCRIBED,
-														 results, presenceEl, settings, roster_util);
 
 					// Broadcast initial presence to other available user resources
 					updateUserResources(presenceEl, session, results, first);