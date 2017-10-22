8ec9b82bd48e81bd8eccd4750a1438755776b184
Andrzej Wójcik <andrzej.wojcik@tigase.net>
2017-10-22 13:34:00 +0200

    #6212: added interface for password recovery

---
+++ server/src/main/java/tigase/auth/PasswordResetterIfc.java
@@ -0,0 +1,31 @@
+/*
+ * Tigase Jabber/XMPP Server
+ * Copyright (C) 2004-2017 "Tigase, Inc." <office@tigase.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://www.gnu.org/licenses/.
+ */
+package tigase.auth;
+
+import tigase.component.exceptions.RepositoryException;
+import tigase.xmpp.BareJID;
+
+public interface PasswordResetterIfc {
+
+	void changePassword(String encodedToken, String password) throws RepositoryException;
+
+	void validateToken(String encodedToken) throws RepositoryException, RuntimeException;
+
+	void sendToken(BareJID bareJID, String url) throws RepositoryException, Exception;
+	
+}
