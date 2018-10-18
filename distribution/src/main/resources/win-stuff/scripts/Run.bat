b3b6e184cb2bf5ff7365d2a0f2dfa0443cc4e97d
Andrzej Wójcik <andrzej.wojcik@tigase.net>
2018-10-18 17:59:45 +0200

    #8286: Added support for execution of upgrade-schema, install-schema and destroy-schema on Windows OS

--- distribution/src/main/resources/win-stuff/scripts/Run.bat
+++ distribution/src/main/resources/win-stuff/scripts/Run.bat
@@ -1,1 +1,29 @@
-java -cp "jars/*" -Djdbc.drivers=com.mysql.jdbc.Driver tigase.server.XMPPServer --property-file etc/init.properties
+@echo off
+set task=%1
+set filename=%0
+
+shift
+shift
+set args=%1
+shift
+:start
+if [%1] == [] goto done
+set args=%args% %1
+shift
+goto start
+
+:schemaManager
+java -cp "jars/*" -Djdbc.drivers=com.mysql.jdbc.Driver -DscriptName='%filename%' tigase.db.util.SchemaManager %task% %args%
+goto finish
+
+:done
+
+set cmd = 
+set class="tigase.server.XMPPServer"
+if "%task%" == "upgrade-schema" goto schemaManager
+if "%task%" == "install-schema" goto schemaManager
+if "%task%" == "destroy-schema" goto schemaManager
+
+java -cp "jars/*" -Djdbc.drivers=com.mysql.jdbc.Driver tigase.server.XMPPServer %args%
+
+:finish
