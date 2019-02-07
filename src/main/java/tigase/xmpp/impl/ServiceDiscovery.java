d20b1f949e65c28cc1adbbdaca3d3eae0b159bfe
Andrzej Wójcik <andrzej.wojcik@tigase.net>
2019-02-07 16:10:34 +0100

    #8562: account owner service discovery

--- src/main/java/tigase/xmpp/impl/ServiceDiscovery.java
+++ src/main/java/tigase/xmpp/impl/ServiceDiscovery.java
@@ -20,14 +20,18 @@ package tigase.xmpp.impl;
 import tigase.db.NonAuthUserRepository;
 import tigase.disco.XMPPServiceCollector;
 import tigase.kernel.beans.Bean;
+import tigase.kernel.beans.Inject;
 import tigase.server.Command;
 import tigase.server.Iq;
 import tigase.server.Packet;
 import tigase.server.xmppsession.SessionManager;
+import tigase.vhosts.VHostManagerIfc;
 import tigase.xml.Element;
 import tigase.xmpp.*;
 import tigase.xmpp.jid.JID;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.logging.Level;
@@ -53,24 +57,111 @@ public class ServiceDiscovery
 			new Element("feature", new String[]{"var"}, new String[]{XMPPServiceCollector.INFO_XMLNS}),
 			new Element("feature", new String[]{"var"}, new String[]{XMPPServiceCollector.ITEMS_XMLNS})};
 
-	@Override
-	public Authorization canHandle(Packet packet, XMPPResourceConnection conn) {
-		if (packet.isServiceDisco()) {
-			try {
-				if (packet.getStanzaTo() != null && packet.getStanzaTo().getLocalpart() != null &&
-						packet.getStanzaTo().getResource() == null &&
-						(conn == null || conn.isUserId(packet.getStanzaTo().getBareJID()))) {
-					return null;
-				}
-			} catch (NotAuthorizedException ex) {
-			}
-		}
-		return super.canHandle(packet, conn);
-	}
+	@Inject(nullAllowed = true)
+	private AccountServiceProvider serviceProvider;
 
+	@Inject(nullAllowed = true)
+	private SessionManager sessionManager;
+	@Inject
+	private VHostManagerIfc vhostManager;
+	
 	@Override
 	public String id() {
 		return ID;
+	}
+
+	@Override
+	public void process(Packet packet, XMPPResourceConnection session, NonAuthUserRepository repo,
+						Queue<Packet> results, Map<String, Object> settings) throws XMPPException {
+
+		JID to = packet.getStanzaTo();
+
+		if (to != null) {
+			JID from = packet.getStanzaFrom();
+			if (from == null) {
+				if (session == null || !session.isAuthorized()) {
+					log.log(Level.FINEST, "got <iq/> packet with no 'from' attribute = {0}", packet);
+					return;
+				}
+				from = session.getjid();
+			}
+
+			if (packet.getType() == StanzaType.get && to.getLocalpart() != null && to.getResource() == null &&
+					sessionManager.isLocalDomain(to.getDomain())) {
+				Element query = packet.getElement()
+						.findChild(el -> el.getName() == "query" &&
+								el.getXMLNS() == "http://jabber.org/protocol/disco#info");
+				if (serviceProvider != null && !isLocalComponent(to)) {
+					if (query != null && query.getAttributeStaticStr("node") == null) {
+						// custom handling..
+						Packet result = packet.copyElementOnly();
+						result.setPacketTo(serviceProvider.getServiceProviderComponentJid());
+						result.setPacketFrom(sessionManager.getComponentId());
+						results.offer(result);
+					} else {
+						// this packet is to local user (offline or not but to bare JID) - just forward to service provider component
+						Packet result = packet.copyElementOnly();
+						if (packet.getStanzaTo() == null && session != null) {
+							// in case if packet is from local user without from/to
+							JID userJid = JID.jidInstance(session.getBareJID());
+							result.initVars(packet.getStanzaFrom() != null ? packet.getStanzaFrom() : session.getJID(),
+											userJid);
+						}
+						if (packet.getStanzaFrom() == null) {
+							// at this point we should already have "from" attribute set
+							// if we do not have it, then we should drop this packet
+							if (log.isLoggable(Level.FINEST)) {
+								log.log(Level.FINEST,
+										"received <iq/> packet to forward to service provider component without 'from' attribute, dropping packet = {0}",
+										packet);
+							}
+							return;
+						}
+						result.setPacketFrom(packet.getFrom());
+						result.setPacketTo(serviceProvider.getServiceProviderComponentJid());
+
+						results.offer(result);
+					}
+				} else if (query != null && query.getAttributeStaticStr("node") == null) {
+					query = new Element("query");
+					query.setXMLNS("http://jabber.org/protocol/disco#info");
+					Packet result = packet.okResult(query, 0);
+					addAccountFeatures(result, from.getBareJID().equals(to.getBareJID()));
+					results.offer(result);
+				} else {
+					super.process(packet, session, repo, results, settings);
+				}
+			} else if ((packet.getType() == StanzaType.result || packet.getType() == StanzaType.error) &&
+					from.getLocalpart() != null && from.getResource() == null &&
+					sessionManager.isLocalDomain(from.getDomain())) {
+				if (serviceProvider != null && packet.getPacketFrom() != null &&
+						packet.getPacketFrom().equals(serviceProvider.getServiceProviderComponentJid())) {
+					if (session != null && session.isUserId(to.getBareJID())) {
+						Packet result = packet.copyElementOnly();
+						result.setPacketFrom(packet.getFrom());
+						result.setPacketTo(session.getConnectionId());
+						addAccountFeatures(result, from.getBareJID().equals(to.getBareJID()));
+						results.offer(result);
+					} else {
+						if (sessionManager.isLocalDomain(to.getDomain())) {
+							return;
+						} else {
+							Packet result = packet.copyElementOnly();
+							result.setPacketFrom(packet.getFrom());
+							result.setPacketTo(null);
+							addAccountFeatures(result, false);
+							results.offer(result);
+						}
+					}
+				} else {
+					super.process(packet, session, repo, results, settings);
+				}
+			} else {
+				super.process(packet, session, repo, results, settings);
+			}
+		} else {
+			super.process(packet, session, repo, results, settings);
+		}
 	}
 
 	@Override
@@ -114,5 +205,34 @@ public class ServiceDiscovery
 	@Override
 	public String[] supNamespaces() {
 		return XMLNSS;
+	}
+
+	private boolean isLocalComponent(JID jid) {
+		return vhostManager.isLocalDomainOrComponent(jid.getLocalpart() + "." + jid.getDomain());
+	}
+
+	private void addAccountFeatures(Packet result, boolean isOwner) {
+		if (result.getType() != StanzaType.result) {
+			return;
+		}
+		Element query = result.getElement().getChild("query");
+		if (query != null) {
+			if (isOwner) {
+				List<Element> features = query.getChildren();
+				query.setChildren(Collections.emptyList());
+				query.addChild(new Element("identity", new String[]{"category", "type"},
+										   new String[]{"account", "registered"}));
+				if (features != null) {
+					features.forEach(query::addChild);
+				}
+				sessionManager.getDiscoFeatures(result.getStanzaFrom()).forEach(query::addChild);
+			}
+		}
+	}
+
+	public interface AccountServiceProvider {
+
+		JID getServiceProviderComponentJid();
+
 	}
 }    // ServiceDiscovery
