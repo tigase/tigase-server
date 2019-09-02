783a11b4e7150ff3f4568e02009ebbba48902351
Wojciech Kapcia <wojtek@tigase.org>
2019-09-02 16:54:47 -0400

    Fix String.formatter parameter substitution; #server-1071

--- src/main/java/tigase/server/ConnectionManager.java
+++ src/main/java/tigase/server/ConnectionManager.java
@@ -1209,7 +1209,9 @@ public abstract class ConnectionManager<IO extends XMPPIOService<?>>
 					// Accept side for component service is not ready yet?
 					// Let's wait for a few secs and try again.
 					if (log.isLoggable(Level.FINEST)) {
-						log.log(Level.FINEST, String.format("Problem reconnecting the service: %1$s, port_props: %2%s", serv, port_props), e);
+						log.log(Level.FINEST,
+								String.format("Problem reconnecting the service: %1$s, port_props: %2$s", serv,
+											  port_props), e);
 					}
 					updateConnectionDetails(port_props);
 
