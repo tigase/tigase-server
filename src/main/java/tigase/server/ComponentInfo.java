ee8d532ae59c0eb625c6233d9245e676355f7f70
Wojciech Kapcia <wojtek@tigase.org>
2020-01-21 13:06:54 +0100

    Add more detail to debug log when checking classes for version; #helpdeskpr-391

--- src/main/java/tigase/server/ComponentInfo.java
+++ src/main/java/tigase/server/ComponentInfo.java
@@ -84,7 +84,7 @@ public class ComponentInfo {
 			try {
 				return Class.forName(clz);
 			} catch (Exception e) {
-				log.log(Level.INFO, "Problem obtaining version for class: " + clz);
+				log.log(Level.INFO, "Problem obtaining version for class: " + clz + ", exception: " + e.getMessage());
 			}
 			return null;
 		}).filter(Objects::nonNull).map(clz -> {
