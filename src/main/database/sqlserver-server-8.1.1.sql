0d4a955a15ab2c800363c95f4a1891e96a7b72b0
Wojciech Kapcia <wojtek@tigase.org>
2020-09-25 18:53:52 -0300

    Bump stable version, add required schema version files; #server-1213

---
+++ src/main/database/sqlserver-server-8.1.1.sql
@@ -0,0 +1,18 @@
+--
+-- Tigase XMPP Server - The instant messaging server
+-- Copyright (C) 2004 Tigase, Inc. (office@tigase.com)
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Affero General Public License as published by
+-- the Free Software Foundation, version 3 of the License.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Affero General Public License for more details.
+--
+-- You should have received a copy of the GNU Affero General Public License
+-- along with this program. Look for COPYING file in the top folder.
+-- If not, see http://www.gnu.org/licenses/.
+--
+
