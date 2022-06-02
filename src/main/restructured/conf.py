674b55e77fc568e887de4293ac27327a1d4e3c23
Qian Luo <qianqianluo@gmail.com>
2022-06-02 11:47:36 -0700

    added change due to maven run

--- src/main/restructured/conf.py
+++ src/main/restructured/conf.py
@@ -1,3 +1,21 @@
+#
+# Tigase MIX - MIX component for Tigase
+# Copyright (C) 2020 Tigase, Inc. (office@tigase.com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. Look for COPYING file in the top folder.
+# If not, see http://www.gnu.org/licenses/.
+#
+
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
--- src/main/restructured/make.bat
+++ src/main/restructured/make.bat
@@ -1,3 +1,21 @@
+@REM
+@REM Tigase MIX - MIX component for Tigase
+@REM Copyright (C) 2020 Tigase, Inc. (office@tigase.com)
+@REM
+@REM This program is free software: you can redistribute it and/or modify
+@REM it under the terms of the GNU Affero General Public License as published by
+@REM the Free Software Foundation, version 3 of the License.
+@REM
+@REM This program is distributed in the hope that it will be useful,
+@REM but WITHOUT ANY WARRANTY; without even the implied warranty of
+@REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+@REM GNU Affero General Public License for more details.
+@REM
+@REM You should have received a copy of the GNU Affero General Public License
+@REM along with this program. Look for COPYING file in the top folder.
+@REM If not, see http://www.gnu.org/licenses/.
+@REM
+
 @ECHO OFF
 
 pushd %~dp0
