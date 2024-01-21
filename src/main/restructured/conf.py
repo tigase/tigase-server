235a197c3ed2199ce04cf1fd2d1f54387dc11cb0
Andrzej Wójcik <andrzej.wojcik@tigase.net>
2024-01-21 12:28:30 +0100

    Fixing documentation building

--- src/main/restructured/conf.py
+++ src/main/restructured/conf.py
@@ -48,6 +48,9 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+extensions = [
+    'sphinx_rtd_theme'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
